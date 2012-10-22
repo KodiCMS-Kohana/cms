424fc7cf2d061efffe0b1fe2670da37e844020ff
ButscH <butschster@gmail.com>
2012-10-22 13:26:34 +0400

    Поправлен шаблон страницы настроек

--- cms/app/views/setting/index.php
+++ cms/app/views/setting/index.php
@@ -2,83 +2,88 @@
 	<h1><?php echo __( 'General setting' ); ?></h1>
 </div>
 
-<form id="settingForm" class="form-horizontal" action="<?php echo URL::site( 'setting' ); ?>" method="post">
-	
+<?php echo form::open('setting', array(
+	'id' => 'settingForm', 'class' => 'form-horizontal'
+)); ?>
+
 	<?php echo Form::hidden('token', Security::token()); ?>
 	
-	<fieldset>
-		<legend><?php echo __( 'Site options' ); ?></legend>
+	<div class="well well-small">
+		<fieldset>
+			<legend><?php echo __( 'Site options' ); ?></legend>
 
-		<div class="control-group">
-			<label class="control-label title" for="settingTitle"><?php echo __( 'Site title' ); ?></label>
-			<div class="controls">
-				<?php
-				echo Form::input( 'setting[admin_title]', Setting::get( 'admin_title' ), array(
-					'class' => 'input-xlarge title', 'id' => 'settingTitle'
-				) );
-				?>
+			<div class="control-group">
+				<label class="control-label title" for="settingTitle"><?php echo __( 'Site title' ); ?></label>
+				<div class="controls">
+					<?php
+					echo Form::input( 'setting[admin_title]', Setting::get( 'admin_title' ), array(
+						'class' => 'input-title', 'id' => 'settingTitle'
+					) );
+					?>
 
-				<p class="help-block"><?php echo __( 'This text will be bresent at backend and can be used in themes.' ); ?></p>
+					<p class="help-block"><?php echo __( 'This text will be bresent at backend and can be used in themes.' ); ?></p>
+				</div>
 			</div>
-		</div>
-		
-		<div class="control-group">
-			<?php echo Form::label('setting_date_format', __('Date format'), array('class' => 'control-label')); ?>
-			<div class="controls">
-				<?php
-				echo Form::select('setting[date_format]', array(
-					'Y-m-d' => '2011-12-14',
-					'd.m.Y' => '14.12.2011',
-					"Y/m/d" => "2011/12/14",
-					"m/d/Y" => "12/14/2011",
-					"d/m/Y" => "14/12/2011",
-					"d F Y" => '14 декабря 2011'
-				), Setting::get('date_format'), array('id' => 'setting_date_format'));
-				?>
-			</div>
-		</div>
 
-		<div class="control-group">
-			<label class="control-label" for="settingSection"><?php echo __( 'Default backend section' ); ?></label>
-			<div class="controls">
-				<select id="settingSection" name="setting[default_tab]">
-					<?php $current_default_nav = Setting::get( 'default_tab' ); ?>
-					<?php foreach ( Model_Navigation::get() as $section ): ?>
-						<optgroup label="<?php echo $section->name(); ?>">
-							<?php foreach ( $section->get_pages() as $item ): ?>
-							<?php $tab = trim(str_replace(ADMIN_DIR_NAME, '', $item->url()), '/'); ?>
-							<option value="<?php echo $tab; ?>" <?php if ( $tab == $current_default_nav ) echo 'selected="selected"'; ?> ><?php echo $item->name(); ?></option>
-							<?php endforeach; ?>
-						</optgroup>
-					<?php endforeach; ?>
-				</select>
-
-				<p class="help-block"><?php echo __( 'This allows you to specify which section you will see by default after login.' ); ?></p>
+			<div class="control-group">
+				<?php echo Form::label('setting_date_format', __('Date format'), array('class' => 'control-label')); ?>
+				<div class="controls">
+					<?php
+					echo Form::select('setting[date_format]', array(
+						'Y-m-d' => '2011-12-14',
+						'd.m.Y' => '14.12.2011',
+						"Y/m/d" => "2011/12/14",
+						"m/d/Y" => "12/14/2011",
+						"d/m/Y" => "14/12/2011",
+						"d F Y" => '14 декабря 2011'
+					), Setting::get('date_format'), array('id' => 'setting_date_format'));
+					?>
+				</div>
 			</div>
-		</div>
 
-		<div class="control-group">
-			<label class="control-label" for="settingProfiling"><?php echo __( 'Profiling' ); ?></label>
-			<div class="controls">
-				<?php
-				echo Form::select( 'setting[profiling]', array( 'yes' => __( 'Yes' ), 'no' => __( 'No' ) ), Setting::get( 'profiling' ), array(
-					'id' => 'settingProfiling'
-				) );
-				?>
-			</div>
-		</div>
-		
-		<div class="control-group">
-			<label class="control-label" for="settingDebugMode"><?php echo __( 'Debug mode' ); ?></label>
-			<div class="controls">
-				<?php
-				echo Form::select( 'setting[debug]', array( 'yes' => __( 'Yes' ), 'no' => __( 'No' ) ), Setting::get( 'debug' ), array(
-					'id' => 'settingDebugMode'
-				) );
-				?>
-			</div>
-		</div>
+			<div class="control-group">
+				<label class="control-label" for="settingSection"><?php echo __( 'Default backend section' ); ?></label>
+				<div class="controls">
+					<select id="settingSection" name="setting[default_tab]">
+						<?php $current_default_nav = Setting::get( 'default_tab' ); ?>
+						<?php foreach ( Model_Navigation::get() as $section ): ?>
+							<optgroup label="<?php echo $section->name(); ?>">
+								<?php foreach ( $section->get_pages() as $item ): ?>
+								<?php $tab = trim(str_replace(ADMIN_DIR_NAME, '', $item->url()), '/'); ?>
+								<option value="<?php echo $tab; ?>" <?php if ( $tab == $current_default_nav ) echo 'selected="selected"'; ?> ><?php echo $item->name(); ?></option>
+								<?php endforeach; ?>
+							</optgroup>
+						<?php endforeach; ?>
+					</select>
 
+					<p class="help-block"><?php echo __( 'This allows you to specify which section you will see by default after login.' ); ?></p>
+				</div>
+			</div>
+
+			<div class="control-group">
+				<label class="control-label" for="settingProfiling"><?php echo __( 'Profiling' ); ?></label>
+				<div class="controls">
+					<?php
+					echo Form::select( 'setting[profiling]', array( 'yes' => __( 'Yes' ), 'no' => __( 'No' ) ), Setting::get( 'profiling' ), array(
+						'id' => 'settingProfiling'
+					) );
+					?>
+				</div>
+			</div>
+
+			<div class="control-group">
+				<label class="control-label" for="settingDebugMode"><?php echo __( 'Debug mode' ); ?></label>
+				<div class="controls">
+					<?php
+					echo Form::select( 'setting[debug]', array( 'yes' => __( 'Yes' ), 'no' => __( 'No' ) ), Setting::get( 'debug' ), array(
+						'id' => 'settingDebugMode'
+					) );
+					?>
+				</div>
+			</div>
+		</fieldset>
+	</div>
+	<div class="well well-small">
 		<fieldset>
 			<legend><?php echo __( 'Page options' ); ?></legend>
 
@@ -120,13 +125,15 @@
 				</div>
 			</div>
 		</fieldset>
+	</div>
 
-		<?php Observer::notify( 'view_setting_plugins' ); ?>
+	<div class="well well-small">
+	<?php Observer::notify( 'view_setting_plugins' ); ?>
+	</div>
 
-		<div class="form-actions">
-			<?php echo Form::button( 'submit', UI::icon( 'ok' ) . ' ' . __( 'Save setting' ), array(
-				'class' => 'btn btn-large btn-success'
-			) ); ?>
-		</div>
-
-</form>
+	<div class="form-actions">
+		<?php echo Form::button( 'submit', UI::icon( 'ok' ) . ' ' . __( 'Save setting' ), array(
+			'class' => 'btn btn-large btn-success'
+		) ); ?>
+	</div>
+<?php Form::close(); ?>
