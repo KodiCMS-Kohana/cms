801acea7a3fd9cb6e49cd1c53c10d18ea95a3f35
ButscH <butschster@gmail.com>
2012-11-05 12:43:44 +0400

    Change version

--- cms/app/bootstrap.php
+++ cms/app/bootstrap.php
@@ -96,7 +96,7 @@ Kohana::init( array(
 
 define('CMS_NAME',			'KodiCMS');
 define('CMS_SITE',			'http://www.kodicms.ru');
-define('CMS_VERSION',		'1.1');
+define('CMS_VERSION',		'1.5.6');
 
 define('BASE_URL',			URL::base('http'));
 define('ADMIN_RESOURCES',	BASE_URL . 'cms/media/');
