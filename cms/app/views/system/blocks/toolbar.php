934d9d5f223bde3394ff4b93cc7facf449df37d5
ButscH <butschster@gmail.com>
2012-11-13 15:55:53 +0400

    Fix

--- cms/app/views/system/blocks/toolbar.php
+++ cms/app/views/system/blocks/toolbar.php
@@ -3,6 +3,6 @@ Observer::notify('front_page_toolbar');
 
 if(Setting::get( 'profiling' ) == 'yes')
 {
-	$inject_html .= (string) View::factory( 'profiler/stats' );
+	echo View::factory( 'profiler/stats' );
 }
 ?>
