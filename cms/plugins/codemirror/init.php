802cad1677f3338a3eeb362feb7f8fc50cef34f2
butschster <butschster@gmail.com>
2013-01-29 11:22:34 +0400

    Регистр названия папки переведен в нижний

--- cms/plugins/codemirror/init.php
+++ cms/plugins/codemirror/init.php
@@ -4,19 +4,19 @@ $plugin = Plugins_Item::factory( array(
 	'id' => 'codemirror',
 	'title' => 'Codemirror',
 	'author' => 'ButscH',
-	'description' => 'CodeMirror is a JavaScript component that provides a code editor in the browser. When a mode is available for the language you are coding in, it will color your code, and optionally help with indentation.',
+	'description' => 'codemirror is a JavaScript component that provides a code editor in the browser. When a mode is available for the language you are coding in, it will color your code, and optionally help with indentation.',
 	'javascripts' => array(
-		'vendors/CodeMirror-3.01/lib/codemirror.js',
-		'vendors/CodeMirror-3.01/mode/clike/clike.js',
-		'vendors/CodeMirror-3.01/mode/xml/xml.js',
-		'vendors/CodeMirror-3.01/mode/javascript/javascript.js',
-		'vendors/CodeMirror-3.01/mode/css/css.js',
-		'vendors/CodeMirror-3.01/mode/htmlmixed/htmlmixed.js',
-		'vendors/CodeMirror-3.01/mode/php/php.js',
+		'vendors/codemirror-3.01/lib/codemirror.js',
+		'vendors/codemirror-3.01/mode/clike/clike.js',
+		'vendors/codemirror-3.01/mode/xml/xml.js',
+		'vendors/codemirror-3.01/mode/javascript/javascript.js',
+		'vendors/codemirror-3.01/mode/css/css.js',
+		'vendors/codemirror-3.01/mode/htmlmixed/htmlmixed.js',
+		'vendors/codemirror-3.01/mode/php/php.js',
 		'codemirror.js'
 	),
 	'css' => array(
-		'vendors/CodeMirror-3.01/lib/codemirror.css',
+		'vendors/codemirror-3.01/lib/codemirror.css',
 		'codemirror.css',
 	)
 ) )
