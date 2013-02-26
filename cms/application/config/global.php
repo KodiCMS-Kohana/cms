5e64df7ec2a60acd62a96ad00be9adbd6fd465da
butschster <butschster@gmail.com>
2013-02-26 10:45:13 +0400

    Увеличено время кеширования страниц + добавлены новые теги для фильтрации

--- cms/application/config/global.php
+++ cms/application/config/global.php
@@ -4,17 +4,19 @@ return array(
 
 	'cache' => array(
 		'page_parts' => 86400,
-		'front_page' => 60,
+		'front_page' => 86400,
 		'tags' => 86400
 	),
 	
 	'allowed_html_tags' => array(
 		'b' => array(), 'strong' => array(),
 		'i' => array(), 'em' => array(),
-		'ol' => array(), 'ul' => array(), 'li' => array(),
-		'p' => array('align' => 1),
+		'ol' => array('class' => 1), 'ul' => array('class' => 1), 'li' => array(),
+		'p' => array('align' => 1, 'class' => 1),
 		'br' => array(), 'hr' => array(),
-		'h3' => array(), 'h4' => array(),
+		'h2' => array(), 'h3' => array(), 'h4' => array(),
+		'a' => array('href' => 1, 'class' => 1),
+		'blockquote' => array('class' => 1),
+		'iframe' => array('width' => 1, 'height' => 1, 'src' => 1, 'allowfullscreen' => 1)
 	),
-
 );
