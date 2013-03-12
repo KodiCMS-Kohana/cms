934f8127990531493dce24b0b8d2fd7efbf8db04
butschster <butschster@gmail.com>
2013-03-12 12:26:25 +0400

    Fix

--- cms/application/views/part/item.php
+++ cms/application/views/part/item.php
@@ -3,7 +3,7 @@
 		<div class="widget-header widget-no-border-radius widget-inverse">
 			<h4 class="part-name" title="<?php echo __('Double click to edit part name.'); ?>"><%=name %></h4><input type="text" class="edit-name" value="<%=name %>" />
 			
-			<% if (is_protected == <?php echo Model_Page_Part::PART_PROTECTED; ?> && is_developer == 1 ) { %>
+			<% if ((is_protected == <?php echo Model_Page_Part::PART_PROTECTED; ?> && is_developer == 1) || is_protected == <?php echo Model_Page_Part::PART_NOT_PROTECTED; ?> ) { %>
 			<div class="widget-options pull-right">
 				<?php echo UI::button(UI::icon( 'cog' ), array(
 					'class' => 'part-options-button btn btn-mini')
@@ -16,7 +16,7 @@
 			<% } %>
 		</div>
 
-		<% if (is_protected == <?php echo Model_Page_Part::PART_PROTECTED; ?> && is_developer == 1 ) { %>
+		<% if ((is_protected == <?php echo Model_Page_Part::PART_PROTECTED; ?> && is_developer == 1) || is_protected == <?php echo Model_Page_Part::PART_NOT_PROTECTED; ?> ) { %>
 		<div class="widget-content part-options">
 			<div class="row-fluid">
 				<div class="span4 item-filter-cont">
@@ -29,14 +29,13 @@
 					</select>
 				</div>
 
-				<?php if ( AuthUser::hasPermission( 'administrator, developer' ) ): ?>
+				<% if ( is_developer == 1 ) { %>
 				<div class="span4">
 					<label class="checkbox inline">
 						<input type="checkbox" name="is_protected" class="is_protected" <% if (is_protected == <?php echo Model_Page_Part::PART_PROTECTED; ?>) { print('checked="checked"')} %>> <?php echo __( 'Is protected' ); ?>
 					</label>
 				</div>
-				<?php endif; ?>
-
+				<% } %>
 
 				<div class="span4 pull-right align-right">
 					<?php echo UI::button(__( 'Remove part :part_name', array( ':part_name' => '<%= name %>' ) ), array(
