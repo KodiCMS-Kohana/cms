b30b19c5d566f49f29ea02db1257cc94790754e1
butschster <butschster@gmail.com>
2013-04-17 17:24:10 +0400

    jquery browser support

--- cms/media/js/backend.js
+++ cms/media/js/backend.js
@@ -539,3 +539,10 @@ $.fn.serializeObject = function()
     });
     return o;
 };
+
+/*Browser detection patch*/
+jQuery.browser = {};
+jQuery.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
+jQuery.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
