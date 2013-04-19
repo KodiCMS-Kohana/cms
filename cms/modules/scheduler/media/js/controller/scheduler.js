00bf140ae7d650cd3aec33a093031c0aba63cb21
butschster <butschster@gmail.com>
2013-04-19 09:20:26 +0400

    Локализация календаря планировщика, перенос media файлов в модуль планировщика и обновление до последней версии

--- cms/modules/scheduler/media/js/controller/scheduler.js
+++ cms/modules/scheduler/media/js/controller/scheduler.js
@@ -20,6 +20,19 @@ cms.init.add(['scheduler_index'], function () {
 		month: dateMonth,
 		date: dateDay,
 		allDaySlot: true,
+		monthNames: [__('January'),__('February'),__('March'),__('April'),__('May'),__('June'),__('July'),__('August'),__('September'),__('October'),__('November'),__(
+			'December')],
+		dayNamesShort: [__('Sun'),__('Mon'),__('Tue'),__('Wed'),__('Thu'),__('Fri'),__('Sat')],
+		buttonText: {
+			prev: "<span class='fc-text-arrow'>&lsaquo;</span>",
+			next: "<span class='fc-text-arrow'>&rsaquo;</span>",
+			prevYear: "<span class='fc-text-arrow'>&laquo;</span>",
+			nextYear: "<span class='fc-text-arrow'>&raquo;</span>",
+			today: __('Today'),
+			month: __('Month'),
+			week: __('Week'),
+			day: __('Day')
+		},
 
 		events: function(start, end, callback)
 		{
