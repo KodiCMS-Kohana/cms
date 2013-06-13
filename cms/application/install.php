a5f4728e1cb5e283f8f67980d9fe037dfaa6f990
butschster <butschster@gmail.com>
2013-06-13 18:20:53 +0400

    При переносе ядра в модули в окружении инсталлятора забыл добавить модуль

--- cms/application/install.php
+++ cms/application/install.php
@@ -20,6 +20,7 @@ Cookie::$salt = 'install_system';
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules( array(
+	'kodicms'		=> MODPATH . 'kodicms',		// Core
 	'assets'		=> MODPATH . 'assets',		// Asset Manager
 	'cache'			=> MODPATH . 'cache',		// Cache manager
 	'database'		=> MODPATH . 'database',	// Database access
