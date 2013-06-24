89c9c161fa6058339818d09de1f79de270049ce4
butschster <butschster@gmail.com>
2013-06-24 23:49:19 +0400

    Fix install pasword form

--- cms/media/js/install.js
+++ cms/media/js/install.js
@@ -1,16 +1,15 @@
 $(function() {
+	var password_generator_status = function() {
+		var checkbox = $('#installPasswordGenerateField');
+
+		if(checkbox.is(':checked')){
+			$('#password_form').hide();
+		} else {
+			$('#password_form').show();
+		}
+	}
 	
 	$('#installPasswordGenerateField').change(password_generator_status);
-	
 	password_generator_status();
 })
 
-var password_generator_status = function() {
-	var checkbox = $('#installPasswordGenerateField');
-	
-	if(checkbox.checked()){
-		$('#password_form').hide();
-	} else {
-		$('#password_form').show();
-	}
-}
