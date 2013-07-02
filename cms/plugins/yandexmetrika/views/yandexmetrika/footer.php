1c1f2a540683e760dd0ba1d567514337b5fcde7d
butschster <butschster@gmail.com>
2013-07-02 19:56:29 +0400

    Добавлены дополнительные параметры при настройки Яндекс метрики

--- cms/plugins/yandexmetrika/views/yandexmetrika/footer.php
+++ cms/plugins/yandexmetrika/views/yandexmetrika/footer.php
@@ -1,14 +1,34 @@
+<?php 
+	$options = array();
+	
+	if($plugin->webvisor == 1) $options[] = 'webvisor:true';
+	if($plugin->clickmap == 1) $options[] = 'clickmap:true';
+	if($plugin->track_links == 1) $options[] = 'trackLinks:true';
+	if($plugin->accurate_track_bounce == 1) $options[] = 'accurateTrackBounce:true';
+?>
+
 <!-- Yandex.Metrika counter -->
-<div style="display:none;"><script type="text/javascript">
-(function(w, c) {
-    (w[c] = w[c] || []).push(function() {
-        try {
-            w.yaCounter<?php echo $plugin->counter_id; ?> = new Ya.Metrika({id:<?php echo $plugin->counter_id; ?>, enableAll: true});
-        }
-        catch(e) { }
-    });
-})(window, "yandex_metrika_callbacks");
-</script></div>
-<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
-<noscript><div><img src="//mc.yandex.ru/watch/<?php echo $plugin->counter_id; ?>" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<script type="text/javascript">
+(function (d, w, c) {
+	(w[c] = w[c] || []).push(function() {
+		try {
+			w.yaCounter21680515 = new Ya.Metrika({
+					id:<?php echo $plugin->counter_id ?><?php if(!empty($options)): ?>,<?php echo implode(',', $options); ?><?php endif; ?>
+			});
+		} catch(e) { }
+	});
+
+	var n = d.getElementsByTagName("script")[0],
+		s = d.createElement("script"),
+		f = function () { n.parentNode.insertBefore(s, n); };
+	s.type = "text/javascript";
+	s.async = true;
+	s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+	if (w.opera == "[object Opera]") {
+		d.addEventListener("DOMContentLoaded", f, false);
+	} else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/<?php echo $plugin->counter_id ?>" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
--- cms/plugins/yandexmetrika/views/yandexmetrika/settings.php
+++ cms/plugins/yandexmetrika/views/yandexmetrika/settings.php
@@ -11,3 +11,51 @@
 		</div>
 	</div>
 </div>
+
+<div class="widget-header">
+	<h3><?php echo __('Yandex metrika settings'); ?></h3>
+</div>
+
+<div class="widget-content">
+	<div class="control-group">
+		<?php
+			echo Bootstrap_Form_Element_Control_Group::factory(array(
+				'element' => Bootstrap_Form_Element_Checkbox::factory(array(
+					'name' => 'setting[webvisor]', 'value' => 1
+				))
+				->checked($plugin->webvisor == 1)
+				->label(__('WebVisor'))
+			));
+		?>
+		
+		<?php
+			echo Bootstrap_Form_Element_Control_Group::factory(array(
+				'element' => Bootstrap_Form_Element_Checkbox::factory(array(
+					'name' => 'setting[clickmap]', 'value' => 1
+				))
+				->checked($plugin->clickmap == 1)
+				->label(__('Click map'))
+			));
+		?>
+		
+		<?php
+			echo Bootstrap_Form_Element_Control_Group::factory(array(
+				'element' => Bootstrap_Form_Element_Checkbox::factory(array(
+					'name' => 'setting[track_links]', 'value' => 1
+				))
+				->checked($plugin->track_links == 1)
+				->label(__('External links, file downloads and "Share" button report'))
+			));
+		?>
+
+		<?php
+			echo Bootstrap_Form_Element_Control_Group::factory(array(
+				'element' => Bootstrap_Form_Element_Checkbox::factory(array(
+					'name' => 'setting[accurate_track_bounce]', 'value' => 1
+				))
+				->checked($plugin->accurate_track_bounce == 1)
+				->label(__('Accurate bounce rate'))
+			));
+		?>
+	</div>
+</div>
