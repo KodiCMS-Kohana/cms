086e9c2bc2061380e6771faf95bb7aee313bb9bd
butschster <butschster@gmail.com>
2013-07-02 20:01:14 +0400

    Локализация настроек Яндекс метрики

---
+++ cms/plugins/yandexmetrika/i18n/ru.php
@@ -0,0 +1,13 @@
+<?php defined( 'SYSPATH' ) or die( 'No direct access allowed.' );
+
+return array(
+	'Yandex metrika settings' => 'Настройки метрики',
+	'Metrics ID' => 'ID метрики',
+	'WebVisor' => 'Вебвизор',
+	'Click map' => 'Карта кликов',
+	'External links, file downloads and "Share" button report' => 'Внешние ссылки, загрузки файлов и отчёт по кнопке «Поделиться»',
+	'Accurate bounce rate' => 'Точный показатель отказов',
+	'' => '',
+	'' => '',
+	
+);
