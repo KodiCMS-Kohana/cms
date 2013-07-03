19c4a23a233d0451f807aed6c2e8368b39dd755d
ButscH <butschster@gmail.com>
2013-07-03 11:38:13 +0400

    Исправлена страница настроек плагина "Less compiler"

---
+++ cms/plugins/less/classes/plugin/less.php
@@ -0,0 +1,42 @@
+<?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
+
+class Plugin_Less extends Plugin_Decorator {
+	
+	public function default_settings()
+	{
+		$settings = parent::default_settings();
+		
+		$settings['less_folder_path'] = 'media/less';
+		$settings['css_folder_path'] = 'media/css';
+		$settings['enabled'] = 'no';
+		
+		return $settings;
+	}
+	
+	public function set_settings( array $data )
+	{
+		if( !isset($data['enabled']) ) $data['enabled'] = 'no';
+
+		return parent::set_settings($data);
+	}
+	
+	public function is_dir_lees()
+	{
+		return is_dir( $this->less_path());
+	}
+	
+	public function is_dir_css()
+	{
+		return is_dir( $this->css_path());
+	}
+	
+	public function less_path()
+	{
+		return DOCROOT . trim($this->less_folder_path, '/') . DIRECTORY_SEPARATOR;
+	}
+	
+	public function css_path()
+	{
+		return DOCROOT . trim($this->css_folder_path, '/') . DIRECTORY_SEPARATOR;
+	}
+}
--- cms/plugins/less/views/less/settings.php
+++ cms/plugins/less/views/less/settings.php
@@ -4,7 +4,7 @@
 <div class="widget-content">
 	<div class="control-group">
 		<div class="checkbox">
-			<label><?php echo Form::checkbox('setting[enabled]', 'yes', $plugin->get('enabled', 'no') == 'yes'); ?> <?php echo __('Enable compiler'); ?></label>
+			<label><?php echo Form::checkbox('setting[enabled]', 'yes', $plugin->get('enabled') == 'yes'); ?> <?php echo __('Enable compiler'); ?></label>
 		</div>
 	</div>
 </div>
@@ -12,26 +12,26 @@
 	<h3><?php echo __('Paths'); ?></h3>
 </div>
 <div class="widget-content">
-	<div class="control-group <?php if(!$is_dir_less): ?>error<?php endif; ?>">
+	<div class="control-group <?php if(!$plugin->is_dir_lees()): ?>error<?php endif; ?>">
 		<label class="control-label"><?php echo __('Less folder path'); ?></label>
 		<div class="controls">
-			<?php echo Form::input('setting[less_folder_path]', $plugin->get('less_folder_path', $less_folder_path), array(
+			<?php echo Form::input('setting[less_folder_path]', $plugin->get('less_folder_path'), array(
 				'class' => 'input-xlarge'
 			)); ?>
-			<?php if(!$is_dir_less): ?>
-			<p class="help-block"><?php echo __('Directory :dir not exists', array(':dir' => $less_folder_path)); ?></p>
+			<?php if(!$plugin->is_dir_lees()): ?>
+			<p class="help-block"><?php echo __('Directory :dir not exists', array(':dir' => $plugin->less_path())); ?></p>
 			<?php endif; ?>
 		</div>
 	</div>
 
-	<div class="control-group <?php if(!$is_dir_css): ?>error<?php endif; ?>">
+	<div class="control-group <?php if(!$plugin->is_dir_css()): ?>error<?php endif; ?>">
 		<label class="control-label"><?php echo __('Css folder path'); ?></label>
 		<div class="controls">
 			<?php echo Form::input('setting[css_folder_path]', $plugin->get('css_folder_path', $css_folder_path), array(
 				'class' => 'input-xlarge'
 			)); ?>
-			<?php if(!$is_dir_css): ?>
-			<p class="help-block"><?php echo __('Directory :dir not exists', array(':dir' => $css_folder_path)); ?></p>
+			<?php if(!$plugin->is_dir_css()): ?>
+			<p class="help-block"><?php echo __('Directory :dir not exists', array(':dir' => $plugin->css_path())); ?></p>
 			<?php endif; ?>
 		</div>
 	</div>
