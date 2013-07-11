7d8e19ed16179827621f2f0220d00ef08e14362a
ButscH <butschster@gmail.com>
2013-07-12 02:04:55 +0400

    Обновлен шаблон футера

--- cms/modules/kodicms/views/layouts/blocks/footer.php
+++ cms/modules/kodicms/views/layouts/blocks/footer.php
@@ -1,10 +1,10 @@
 <footer>
 	<div class="container-fluid">
 		<div class="row-fluid">
-			<div class="span8">
+			<div class="span6">
 				<p><?php echo __('Thank you for using :site', array(':site' => HTML::anchor(CMS_SITE, CMS_NAME))); ?></p>
 			</div>
-			<div class="span4 text-right">
+			<div class="span6 text-right">
 				<p>
 				&copy; 2012<?php echo (date('Y') > 2012) ? ' - ' . date('Y') : ''; ?> <?php echo HTML::anchor( CMS_SITE, CMS_NAME ) ?> v<?php echo CMS_VERSION; ?> | 
 				<?php echo __('Powered by :framework v:version :codename', array(
