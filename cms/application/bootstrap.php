6f0b39bb7ba65792109f5f11adbb033612dd8dd2
ButscH <butschster@gmail.com>
2013-07-12 13:52:49 +0400

    fix

--- cms/application/bootstrap.php
+++ cms/application/bootstrap.php
@@ -111,10 +111,12 @@ define('CMS_VERSION',		'5.14.0');
 if(PHP_SAPI != 'cli')
 {
 	define('BASE_URL',		URL::base('http'));
-	define('SITE_HOST',	str_replace('www.', '', $_SERVER['HTTP_HOST']));
+	define('SITE_HOST',		str_replace('www.', '', $_SERVER['HTTP_HOST']));
 }
 
 if( ! defined( 'BASE_URL' ) ) define('BASE_URL', '/');
+if( ! defined( 'SITE_HOST' ) ) define('SITE_HOST', 'test');
+
 define('ADMIN_RESOURCES',	BASE_URL . 'cms/media/');
 
 /**
