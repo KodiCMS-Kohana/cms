4c642f52251ad39450d5a7c1b44d8b6911983158
ButscH <butschster@gmail.com>
2013-08-21 09:57:45 +0400

    Форма отправки писем для демо сайта

--- cms/modules/installer/data/dump.sql
+++ cms/modules/installer/data/dump.sql
@@ -10,7 +10,10 @@ INSERT INTO `TABLE_PREFIX_pages` (`id`, `title`, `slug`, `breadcrumb`, `meta_tit
 (7, 'My second article', 'my-second-article', 'My second article', 'My second article', '', '', 8, '', '', 100, '__DATE__', '__DATE__', NULL, NULL, NULL, 2, 2),
 (8, 'Articles', 'articles', 'Articles', 'Articles', '', '', 1, '', 'archive', 100, '__DATE__', '__DATE__', '__DATE__', NULL, 1, 2, 2),
 (9, 'Third entry', 'third-entry', 'Third entry', 'Third entry', '', '', 8, '0', '0', 100, '__DATE__', '__DATE__', '__DATE__', 1, 1, 3, 2),
-(10, 'Fourth entry', 'fourth-entry', 'Fourth entry', 'Fourth entry', '', '', 8, '0', '0', 100, '__DATE__', '__DATE__', '__DATE__', 1, 1, 4, 2);
+(10, 'Fourth entry', 'fourth-entry', 'Fourth entry', 'Fourth entry', '', '', 8, '0', '0', 100, '__DATE__', '__DATE__', '__DATE__', 1, 1, 4, 2),
+(11, 'Contacts', 'contacts', 'Контакты', 'Контакты', '', '', 1, '0', '0', 100, '__DATE__', '__DATE__', '__DATE__', 1, 1, 5, 2),
+(12, 'Send page', 'send', 'send', 'send', '', '', 11, 'none', '0', 101, '__DATE__', '__DATE__', '__DATE__', 1, 1, 1, 2);
+
 
 INSERT INTO `TABLE_PREFIX_page_parts` (`id`, `name`, `filter_id`, `content`, `content_html`, `page_id`, `is_protected`) VALUES
 (2, 'body', 'redactor', '<h3>Sorry</h3>\n\n<p>This page not found<br>\n</p>\n', '<h3>Sorry</h3>\n\n<p>This page not found<br>\n</p>\n', 2, 0),
@@ -38,7 +41,15 @@ INSERT INTO `TABLE_PREFIX_page_roles` (`page_id`, `role_id`) VALUES
 (7, 4),
 (8, 2),
 (8, 3),
-(8, 4);
+(8, 4),
+(11, 1),
+(11, 2),
+(11, 3),
+(11, 4),
+(12, 1),
+(12, 2),
+(12, 3),
+(12, 4);
 
 INSERT INTO `TABLE_PREFIX_roles` (`id`, `name`, `description`) VALUES
 (1, 'login', 'Login privileges, granted after account confirmation'),
@@ -141,7 +152,13 @@ INSERT INTO `TABLE_PREFIX_page_widgets` (`page_id`, `widget_id`, `block`) VALUES
 (10, 3, 'footer'),
 (10, 1, 'header'),
 (10, 9, 'recent'),
-(10, 4, 'sidebar');
+(10, 4, 'sidebar'),
+(11, 15, 'body'),
+(11, 2, 'bradcrumbs'),
+(11, 3, 'footer'),
+(11, 1, 'header'),
+(11, 4, 'sidebar'),
+(12, 14, 'body');
 
 INSERT INTO `TABLE_PREFIX_widgets` (`id`, `type`, `template`, `name`, `description`, `created_on`, `code`) VALUES
 (1, 'page_menu', 'header', 'Header menu', '', '2013-03-22 18:38:11', 'O:22:"Model_Widget_Page_Menu":13:{s:7:"exclude";a:3:{i:0;s:1:"6";i:1;s:1:"4";i:2;s:1:"2";}s:2:"id";s:2:"10";s:4:"type";s:9:"page_menu";s:4:"name";s:11:"Header menu";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:6:"header";s:15:"template_params";a:0:{}s:5:"block";N;s:7:"caching";b:0;s:14:"cache_lifetime";i:0;s:9:"throw_404";b:0;s:8:"\0*\0_data";a:3:{s:15:"match_all_paths";i:0;s:7:"page_id";s:1:"1";s:8:"continue";s:0:"";}}'),
@@ -156,7 +173,9 @@ INSERT INTO `TABLE_PREFIX_widgets` (`id`, `type`, `template`, `name`, `descripti
 (10, 'page_pages', 'recent-entries', 'Recent entries index page', '', '2013-03-26 10:27:49', 'O:23:"Model_Widget_Page_Pages":17:{s:8:"\0*\0_data";a:4:{s:11:"list_offset";i:1;s:9:"list_size";i:4;s:7:"page_id";s:1:"8";s:8:"continue";s:0:"";}s:2:"id";s:2:"10";s:4:"type";s:10:"page_pages";s:4:"name";s:25:"Recent entries index page";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:14:"recent-entries";s:16:"backend_template";N;s:17:"frontend_template";N;s:12:"use_template";b:1;s:15:"template_params";a:0:{}s:5:"block";N;s:6:"crumbs";b:0;s:7:"caching";b:1;s:14:"cache_lifetime";i:3600;s:9:"throw_404";b:0;s:7:"\0*\0_ctx";O:7:"Context":9:{s:8:"\0*\0_page";N;s:11:"\0*\0_request";N;s:12:"\0*\0_response";N;s:10:"\0*\0_crumbs";N;s:11:"\0*\0_widgets";a:0:{}s:14:"\0*\0_widget_ids";N;s:10:"\0*\0_blocks";a:0:{}s:10:"\0*\0_params";a:0:{}s:14:"\0*\0_injections";a:0:{}}}'),
 (11, 'page_pages', 'last-entry', 'last entry index page', '', '2013-03-26 10:28:58', 'O:23:"Model_Widget_Page_Pages":17:{s:8:"\0*\0_data";a:4:{s:11:"list_offset";i:0;s:9:"list_size";i:1;s:7:"page_id";s:1:"8";s:8:"continue";s:0:"";}s:2:"id";s:2:"11";s:4:"type";s:10:"page_pages";s:4:"name";s:21:"last entry index page";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:10:"last-entry";s:16:"backend_template";N;s:17:"frontend_template";N;s:12:"use_template";b:1;s:15:"template_params";a:0:{}s:5:"block";N;s:6:"crumbs";b:0;s:7:"caching";b:1;s:14:"cache_lifetime";i:3600;s:9:"throw_404";b:0;s:7:"\0*\0_ctx";O:7:"Context":9:{s:8:"\0*\0_page";N;s:11:"\0*\0_request";N;s:12:"\0*\0_response";N;s:10:"\0*\0_crumbs";N;s:11:"\0*\0_widgets";a:0:{}s:14:"\0*\0_widget_ids";N;s:10:"\0*\0_blocks";a:0:{}s:10:"\0*\0_params";a:0:{}s:14:"\0*\0_injections";a:0:{}}}'),
 (12, 'page_pages', 'recent-entries-rss', 'Recent entries RSS', '', '2013-03-26 10:44:33', 'O:23:"Model_Widget_Page_Pages":17:{s:8:"\0*\0_data";a:4:{s:11:"list_offset";i:0;s:9:"list_size";i:10;s:7:"page_id";s:1:"8";s:8:"continue";s:0:"";}s:2:"id";s:2:"12";s:4:"type";s:10:"page_pages";s:4:"name";s:18:"Recent entries RSS";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:18:"recent-entries-rss";s:16:"backend_template";N;s:17:"frontend_template";N;s:12:"use_template";b:1;s:15:"template_params";a:0:{}s:5:"block";N;s:6:"crumbs";b:0;s:7:"caching";b:1;s:14:"cache_lifetime";i:3600;s:9:"throw_404";b:0;s:7:"\0*\0_ctx";O:7:"Context":9:{s:8:"\0*\0_page";N;s:11:"\0*\0_request";N;s:12:"\0*\0_response";N;s:10:"\0*\0_crumbs";N;s:11:"\0*\0_widgets";a:0:{}s:14:"\0*\0_widget_ids";N;s:10:"\0*\0_blocks";a:0:{}s:10:"\0*\0_params";a:0:{}s:14:"\0*\0_injections";a:0:{}}}'),
-(13, 'pagination', 'paginator', 'Постраничная навигация', '', '2013-08-16 14:46:46', 'O:23:"Model_Widget_Pagination":18:{s:8:"\0*\0_data";a:3:{s:9:"query_key";s:4:"page";s:17:"related_widget_id";s:1:"8";s:8:"continue";s:0:"";}s:2:"id";s:2:"13";s:4:"type";s:10:"pagination";s:4:"name";s:43:"Постраничная навигация";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:9:"paginator";s:16:"backend_template";N;s:17:"frontend_template";N;s:12:"use_template";b:1;s:15:"template_params";a:0:{}s:5:"block";N;s:6:"crumbs";b:0;s:7:"caching";b:0;s:14:"cache_lifetime";i:0;s:10:"cache_tags";a:1:{i:0;s:0:"";}s:9:"throw_404";b:0;s:7:"\0*\0_ctx";O:7:"Context":10:{s:8:"\0*\0_page";N;s:11:"\0*\0_request";N;s:12:"\0*\0_response";N;s:10:"\0*\0_crumbs";N;s:11:"\0*\0_widgets";a:0:{}s:14:"\0*\0_widget_ids";N;s:10:"\0*\0_blocks";a:0:{}s:10:"\0*\0_params";a:0:{}s:19:"\0*\0_behavior_router";a:0:{}s:14:"\0*\0_injections";a:0:{}}}');
+(13, 'pagination', 'paginator', 'Постраничная навигация', '', '2013-08-16 14:46:46', 'O:23:"Model_Widget_Pagination":18:{s:8:"\0*\0_data";a:3:{s:9:"query_key";s:4:"page";s:17:"related_widget_id";s:1:"8";s:8:"continue";s:0:"";}s:2:"id";s:2:"13";s:4:"type";s:10:"pagination";s:4:"name";s:43:"Постраничная навигация";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:9:"paginator";s:16:"backend_template";N;s:17:"frontend_template";N;s:12:"use_template";b:1;s:15:"template_params";a:0:{}s:5:"block";N;s:6:"crumbs";b:0;s:7:"caching";b:0;s:14:"cache_lifetime";i:0;s:10:"cache_tags";a:1:{i:0;s:0:"";}s:9:"throw_404";b:0;s:7:"\0*\0_ctx";O:7:"Context":10:{s:8:"\0*\0_page";N;s:11:"\0*\0_request";N;s:12:"\0*\0_response";N;s:10:"\0*\0_crumbs";N;s:11:"\0*\0_widgets";a:0:{}s:14:"\0*\0_widget_ids";N;s:10:"\0*\0_blocks";a:0:{}s:10:"\0*\0_params";a:0:{}s:19:"\0*\0_behavior_router";a:0:{}s:14:"\0*\0_injections";a:0:{}}}'),
+(14, 'sendmail', 'send-mail-template', ' Send mail ( sender )', '', '2013-08-21 09:31:27', 'O:21:"Model_Widget_SendMail":21:{s:10:"\0*\0_errors";a:0:{}s:12:"\0*\0_messages";a:0:{}s:6:"fields";a:3:{i:1;a:5:{s:6:"source";s:1:"2";s:2:"id";s:7:"subject";s:4:"type";s:2:"10";s:9:"validator";s:9:"not_empty";s:5:"error";s:0:"";}i:2;a:5:{s:6:"source";s:1:"2";s:2:"id";s:5:"email";s:4:"type";s:2:"10";s:9:"validator";s:5:"email";s:5:"error";s:0:"";}i:3;a:5:{s:6:"source";s:1:"2";s:2:"id";s:4:"text";s:4:"type";s:2:"20";s:9:"validator";s:0:"";s:5:"error";s:0:"";}}s:8:"\0*\0_data";a:10:{s:12:"allowed_tags";s:12:"<b><i><u><p>";s:4:"from";s:7:"{email}";s:7:"is_html";b:1;s:7:"subject";s:25:"Message from demo kodicms";s:2:"to";s:20:"butschster@gmail.com";s:5:"to_cc";s:0:"";s:6:"to_bcc";s:0:"";s:5:"field";N;s:8:"continue";s:0:"";s:6:"commit";s:0:"";}s:2:"id";s:2:"14";s:4:"type";s:8:"sendmail";s:4:"name";s:21:" Send mail ( sender )";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:18:"send-mail-template";s:16:"backend_template";N;s:17:"frontend_template";N;s:12:"use_template";b:1;s:15:"template_params";a:0:{}s:5:"block";N;s:6:"crumbs";b:0;s:7:"caching";b:0;s:14:"cache_lifetime";i:0;s:10:"cache_tags";a:1:{i:0;s:0:"";}s:9:"throw_404";b:0;s:7:"\0*\0_ctx";O:7:"Context":10:{s:8:"\0*\0_page";N;s:11:"\0*\0_request";N;s:12:"\0*\0_response";N;s:10:"\0*\0_crumbs";N;s:11:"\0*\0_widgets";a:0:{}s:14:"\0*\0_widget_ids";N;s:10:"\0*\0_blocks";a:0:{}s:10:"\0*\0_params";a:0:{}s:19:"\0*\0_behavior_router";a:0:{}s:14:"\0*\0_injections";a:0:{}}}'),
+(15, 'html', 'send-mail-form', 'Send mail ( form )', '', '2013-08-21 09:34:01', 'O:17:"Model_Widget_HTML":18:{s:2:"id";s:2:"15";s:4:"type";s:4:"html";s:4:"name";s:18:"Send mail ( form )";s:11:"description";s:0:"";s:6:"header";s:0:"";s:8:"template";s:14:"send-mail-form";s:16:"backend_template";N;s:17:"frontend_template";N;s:12:"use_template";b:1;s:15:"template_params";a:0:{}s:5:"block";N;s:6:"crumbs";b:0;s:7:"caching";b:0;s:14:"cache_lifetime";i:0;s:10:"cache_tags";a:1:{i:0;s:0:"";}s:9:"throw_404";b:0;s:7:"\0*\0_ctx";O:7:"Context":10:{s:8:"\0*\0_page";N;s:11:"\0*\0_request";N;s:12:"\0*\0_response";N;s:10:"\0*\0_crumbs";N;s:11:"\0*\0_widgets";a:0:{}s:14:"\0*\0_widget_ids";N;s:10:"\0*\0_blocks";a:0:{}s:10:"\0*\0_params";a:0:{}s:19:"\0*\0_behavior_router";a:0:{}s:14:"\0*\0_injections";a:0:{}}s:8:"\0*\0_data";a:1:{s:8:"continue";s:0:"";}}');
 
 
 SET FOREIGN_KEY_CHECKS=1;
