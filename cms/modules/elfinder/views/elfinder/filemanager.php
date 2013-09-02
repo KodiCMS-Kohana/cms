84fdf96f7c008a09098351493bc44266fb629525
ButscH <butschster@gmail.com>
2013-09-02 12:07:05 +0400

    Filemanager container - auto height

--- cms/modules/elfinder/views/elfinder/filemanager.php
+++ cms/modules/elfinder/views/elfinder/filemanager.php
@@ -4,8 +4,18 @@
 			lang: 'ru',
 			url : '/api-elfinder',
             resizable: false,
-			height: 600
+			height: calculateContentHeight()
 		}).elfinder('instance');
+		
+		$(window).resize(function() {
+			var node = elfinder.getUI('node');
+			var h = calculateContentHeight();
+			node.height(h);
+			node.find('.elfinder-navbar')
+				.add(node.find('.elfinder-cwd'))
+				.add(node.find('.elfinder-cwd-wrapper'))
+				.height(h - node.find('.elfinder-toolbar').height() - node.find('.elfinder-statusbar').height() )
+		});
 	});
 </script>
 
