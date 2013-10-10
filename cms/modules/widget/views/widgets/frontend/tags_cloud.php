eda183f52e0d6e2f30e5b47e9447ca7453c26a7d
butschster <butschster@gmail.com>
2013-10-10 16:08:28 +0400

    Поправлен шаблон по умолчанию для виджета "Облако тегов"

--- cms/modules/widget/views/widgets/frontend/tags_cloud.php
+++ cms/modules/widget/views/widgets/frontend/tags_cloud.php
@@ -1,6 +1,8 @@
 <div class="tags_cloud text-center">
 	<?php foreach($tags as $tag => $params): ?>
-	<?php echo HTML::anchor($tag, $tag, array(
+	<?php echo HTML::anchor(URL::query(array(
+		'tag' => $tag
+	)), $tag, array(
 		'style' => "font-size: {$params['size']}px; color: {$params['color']}"
 	)); ?>
 	<?php endforeach; ?>
