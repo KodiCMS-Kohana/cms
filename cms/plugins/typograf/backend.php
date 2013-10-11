60b770f8f2b0ef0e62cd7afd915c69993b55176b
butschster <butschster@gmail.com>
2013-10-11 18:30:30 +0400

    Добавлен плагин типографа http://mdash.ru , который доступен в редакторе redactor.js

---
+++ cms/plugins/typograf/backend.php
@@ -0,0 +1,3 @@
+<?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
+
+Assets::js('typograf', ADMIN_RESOURCES . 'js/typograf.js', 'jquery');
---
+++ cms/plugins/typograf/media/js/typograf.js
@@ -0,0 +1,13 @@
+if (!RedactorPlugins) var RedactorPlugins = {};
+RedactorPlugins.typograf = {
+	init: function ()
+	{
+		var self = this;
+		this.addBtnAfter('formatting', 'clips', 'Typograf', function() {
+			Api.get('typograf', {text: $(self.getParentNode()).html()}, function(response) {
+				if(response.response)
+					$(self.getParentNode()).html(response.response);
+			});
+		});
+	},
+};
