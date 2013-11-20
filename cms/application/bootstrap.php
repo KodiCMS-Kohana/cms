cddb131d7b31d761a7e15e7ab23e270b14101a77
butschster <butschster@gmail.com>
2013-11-20 21:56:02 +0400

    Change version

--- cms/application/bootstrap.php
+++ cms/application/bootstrap.php
@@ -120,7 +120,7 @@ Kohana::init( array(
 
 define('CMS_NAME',			'KodiCMS');
 define('CMS_SITE',			'http://www.kodicms.ru');
-define('CMS_VERSION',		'6.7.27');
+define('CMS_VERSION',		'7.7.27');
 
 if(PHP_SAPI != 'cli')
 {
