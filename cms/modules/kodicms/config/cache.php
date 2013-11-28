820eb14a472cd9c5e106dcb4160ef98d48883564
butschster <butschster@gmail.com>
2013-11-28 11:42:54 +0400

    При выборе в инсталляторе драйвера кеширования Memcachetag сайт переставал работать, т.к. в конфиге для этого драйвера не был указан конфиг, думаю сейчас данная проблема должна решиться

--- cms/modules/kodicms/config/cache.php
+++ cms/modules/kodicms/config/cache.php
@@ -25,4 +25,21 @@ return array
 		'driver'             => 'apc',
 		'default_expire'     => 3600,
 	),
+	'memcachetag' => array(
+		'driver'             => 'memcachetag',
+		'default_expire'     => 3600,
+		'compression'        => FALSE,              // Use Zlib compression (can cause issues with integers)
+		'servers'            => array(
+			'local' => array(
+				'host'             => 'localhost',  // Memcache Server
+				'port'             => 11211,        // Memcache port number
+				'persistent'       => FALSE,        // Persistent connection
+				'weight'           => 1,
+				'timeout'          => 1,
+				'retry_interval'   => 15,
+				'status'           => TRUE,
+			),
+		),
+		'instant_death'      => TRUE,
+	),
 );
