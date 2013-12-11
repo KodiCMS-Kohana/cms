a378a6db2c6f7ac4404593c9ce8b55e41988d873
butschster <butschster@gmail.com>
2013-12-11 10:39:31 +0400

    Add links to disqus faq

---
+++ cms/plugins/disqus/views/widgets/backend/disqus_comments.php
@@ -0,0 +1,5 @@
+<div class="widget-content">
+	<?php echo HTML::anchor('http://help.disqus.com/customer/portal/articles/472098-javascript-configuration-variables', 'Disqus FAQ', array(
+		'class' => 'popup fancybox.iframe btn btn-info'
+	)); ?>
+</div>
---
+++ cms/plugins/disqus/views/widgets/backend/disqus_counter.php
@@ -0,0 +1,5 @@
+<div class="widget-content">
+	<?php echo HTML::anchor('http://help.disqus.com/customer/portal/articles/565624-tightening-your-disqus-integration', 'Disqus FAQ', array(
+		'class' => 'popup fancybox.iframe btn btn-info'
+	)); ?>
+</div>
