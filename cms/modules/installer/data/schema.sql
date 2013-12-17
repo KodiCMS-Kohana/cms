b63942396e6014ea36569a8e0a6b080fed452b7e
butschster <butschster@gmail.com>
2013-12-17 10:05:13 +0400

    fix

--- cms/modules/installer/data/schema.sql
+++ cms/modules/installer/data/schema.sql
@@ -245,13 +245,13 @@ CREATE TABLE IF NOT EXISTS `__TABLE_PREFIX__logs` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS __TABLE_PREFIX__`email_templates` (
+CREATE TABLE IF NOT EXISTS `__TABLE_PREFIX__email_templates` (
   `id` int(18) NOT NULL AUTO_INCREMENT,
   `created_on` datetime DEFAULT NULL,
   `email_type` int(5) unsigned NOT NULL,
   `status` int(1) NOT NULL DEFAULT '1',
-  `email_from` varchar(255) NOT NULL DEFAULT '{email_from}',
-  `email_to` varchar(255) NOT NULL DEFAULT '{email_to}',
+  `email_from` varchar(255) NOT NULL DEFAULT '',
+  `email_to` varchar(255) NOT NULL DEFAULT '',
   `subject` varchar(255) DEFAULT NULL,
   `message` text,
   `message_type` varchar(5) NOT NULL DEFAULT 'html',
