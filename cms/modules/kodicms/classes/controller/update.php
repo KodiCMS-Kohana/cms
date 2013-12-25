718cccba6da5396e6f71f87f911816759154ab2f
butschster <butschster@gmail.com>
2013-12-25 17:46:29 +0400

    fix

--- cms/modules/kodicms/classes/controller/update.php
+++ cms/modules/kodicms/classes/controller/update.php
@@ -1,27 +1,3 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct access allowed.' );
 
-class Controller_Update extends Controller_System_Backend {
-	
-	public function before()
-	{
-		parent::before();
-
-		$this->breadcrumbs
-			->add(__('Update'), Route::url('backend', array('controller' => 'update')));
-	}
-	
-	public function action_index() 
-	{
-		$this->template->title = __('Update');
-		
-		$db_sql = Install::schema();
-		$file_sql = Install::install_schema();
-
-		$compare = new Database_Helper;
-		$diff = $compare->get_updates($db_sql, $file_sql, TRUE);
-		
-		$this->template->content = View::factory( 'update/index', array(
-			'actions' => $diff,
-		) );
-	}
-}
+class Controller_Update extends KodiCMS_Controller_Update {}
---
+++ cms/modules/kodicms/classes/kodicms/controller/update.php
@@ -0,0 +1,27 @@
+<?php defined( 'SYSPATH' ) or die( 'No direct access allowed.' );
+
+class KodiCMS_Controller_Update extends Controller_System_Backend {
+	
+	public function before()
+	{
+		parent::before();
+
+		$this->breadcrumbs
+			->add(__('Update'), Route::url('backend', array('controller' => 'update')));
+	}
+	
+	public function action_index() 
+	{
+		$this->template->title = __('Update');
+		
+		$db_sql = Database_Helper::schema();
+		$file_sql = Database_Helper::install_schema();
+
+		$compare = new Database_Helper;
+		$diff = $compare->get_updates($db_sql, $file_sql, TRUE);
+		
+		$this->template->content = View::factory( 'update/index', array(
+			'actions' => $diff,
+		) );
+	}
+}
