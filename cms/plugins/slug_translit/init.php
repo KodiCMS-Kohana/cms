ea9217d0b18057cef33d628a173c98a402aa712e
butschster <butschster@gmail.com>
2014-01-08 02:19:22 +0400

    Fix

--- cms/plugins/slug_translit/init.php
+++ cms/plugins/slug_translit/init.php
@@ -2,5 +2,5 @@
 
 Plugin::factory('slug_translit', array(
 	'title' => 'Slug translit',
-	'description' => 'Реформал – это простой и эффективный сервис обратной связи',
+	'description' => '',
 ))->register();
