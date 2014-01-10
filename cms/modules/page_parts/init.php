b90d43c196ad5aee9a912d0eb632f87d9a3ff9b7
butschster <butschster@gmail.com>
2014-01-10 19:23:28 +0400

    upd

--- cms/modules/page_parts/init.php
+++ cms/modules/page_parts/init.php
@@ -51,7 +51,16 @@ Observer::observe('page_edit_after_save', function($page) {
 			->save();
 	}
 	
-	Search::add_to_index('pages', $page->id, $page->title, $indexable_content);
+	if(in_array($page->status_id, Model_Page_Front::get_statuses()))
+	{
+		Search::add_to_index('pages', $page->id, $page->title, $indexable_content, array(
+			'uri' => $page->get_uri()
+		));
+	}
+	else
+	{
+		Search::remove_from_index('pages', $page->id);
+	}
 });
 
 Observer::observe('update_search_index', function($page) {
