9f1431f5ffc7ca10e5d9cb4f63d5a59cc03bc53a
butschster <butschster@gmail.com>
2014-01-10 13:24:17 +0400

    fix

--- cms/modules/kodicms/install/schema.sql
+++ cms/modules/kodicms/install/schema.sql
@@ -35,18 +35,3 @@ CREATE TABLE IF NOT EXISTS `__TABLE_PREFIX__pages` (
   CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`created_by_id`) REFERENCES `__TABLE_PREFIX__users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `pages_ibfk_2` FOREIGN KEY (`updated_by_id`) REFERENCES `__TABLE_PREFIX__users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
-
-CREATE TABLE IF NOT EXISTS `__TABLE_PREFIX__page_parts` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  `filter_id` varchar(25) DEFAULT NULL,
-  `content` longtext,
-  `content_html` longtext,
-  `page_id` int(11) unsigned DEFAULT NULL,
-  `is_protected` tinyint(4) DEFAULT '0',
-  `is_expanded` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `page_id` (`page_id`),
-  KEY `name` (`name`),
-  CONSTRAINT `page_parts_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `__TABLE_PREFIX__pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
