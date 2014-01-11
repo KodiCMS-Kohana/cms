cfcf2a58bc6adebf24c8adf15cc99f6b79a344de
butschster <butschster@gmail.com>
2014-01-12 03:30:43 +0400

    Исправлена ошибка с сохранением виджетов на странице их месторасположения

--- cms/modules/widget/classes/widget/manager.php
+++ cms/modules/widget/classes/widget/manager.php
@@ -256,11 +256,12 @@ class Widget_Manager {
 			$i = 0;
 			foreach($data as $page_id => $block)
 			{
-				if(empty($block['name'])) continue;
+				if($data['block'] == -1) continue;
 
 				$insert->values(array(
 					$page_id, (int) $widget_id, $block['name'], (int) $block['position']
 				));
+
 				$i++;
 			}
 			
