a041c3814f1817d03288b3f917da91b26fcd3029
butschster <butschster@gmail.com>
2014-01-17 13:43:36 +0400

    Fix translations

--- cms/modules/widget/views/widgets/page/edit.php
+++ cms/modules/widget/views/widgets/page/edit.php
@@ -11,6 +11,7 @@
 	<?php if(empty($page->id)): ?>
 	<h4><?php echo __('Copy widgets from'); ?></h4>
 	<select name="widgets[from_page_id]" class="span12">
+		<option value=""><?php echo __('--- Do not copy ---'); ?></option>
 		<?php foreach ($pages as $p): ?>
 		<option value="<?php echo($p['id']); ?>" <?php echo($p['id'] == $page->parent_id ? ' selected="selected"': ''); ?> ><?php echo str_repeat('- ', $p['level'] * 2).$p['title']; ?></option>
 		<?php endforeach; ?>
