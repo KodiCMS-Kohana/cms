eed6f1f4937555ca1a77f8b813612b6ccb3d4627
butschster <butschster@gmail.com>
2014-01-17 14:15:22 +0400

    При выбросе иклюения Странца не найдена добавлена возможность указания текста ошибки, которая к этому привела. Также в плагине "Страница не найдена" изменен алгоритм генерации ошибки

--- cms/plugins/page_not_found/classes/http/exception/front/404.php
+++ cms/plugins/page_not_found/classes/http/exception/front/404.php
@@ -1,25 +1,24 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
 
-class HTTP_Exception_404 extends Kohana_HTTP_Exception_404 
+class HTTP_Exception_Front_404 extends Kohana_HTTP_Exception_404 
 {
 	public function get_response()
 	{
-		$page = DB::select()
+		$page_slug = DB::select('slug')
 			->from(Model_Page::TABLE_NAME)
 			->where('behavior_id', '=', 'page_not_found')
 			->limit(1)
-			->as_object()
 			->execute()
-			->current();
+			->get('slug');
 		
-		if($page)
+		if( ! empty($page_slug) )
 		{
-			$page = Model_Page_Front::find( $page->slug );
+			$page = Model_Page_Front::find( $page_slug );
 
-			// if we fund it, display it!
-			if( is_object($page) )
-			{				
+			if( $page instanceof Model_Page_Front )
+			{
 				return Request::factory($page->url)
+					->query('message', $this->message)
 					->execute();
 			}
 		}
