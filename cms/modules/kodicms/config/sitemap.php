0583e3ade6b3713fb84d1b26f7304b9d84333800
butschster <butschster@gmail.com>
2014-01-31 15:23:36 +0400

    Перенос пункта меню в модуль pages

--- cms/modules/kodicms/config/sitemap.php
+++ cms/modules/kodicms/config/sitemap.php
@@ -33,16 +33,4 @@ return array(
 			)
 		)
 	),
-	array(
-		'name' => 'Content',
-		'children' => array(
-			array(
-				'name' => __('Pages'),
-				'url' => URL::backend('page'),
-				'permissions' => 'page.index',
-				'priority' => 100,
-				'icon' => 'sitemap'
-			)
-		)
-	),
 );
