0d3f5498d82ff9b9d4bbbb4299c32f42b1b69ecf
Warren E. White <wwhite@nerret-tech.com>
2014-03-14 22:10:25 -0700

    Removed static text, added __().

--- cms/plugins/test/init.php
+++ cms/plugins/test/init.php
@@ -1,7 +1,7 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
 
 Plugin::factory('test', array(
-	'title' => 'Тестовый сайт',
+	'title' => __('Test Site'),
 	'version' => '1.0.0',
 	'description' => 'DO NOT INSTALL TO PRODUCTION SERVER',
 ))->register();
