1be9d5fc623586687c5a4c9993d845a658a15a47
Warren E. White <wwhite@nerret-tech.com>
2014-03-21 02:17:37 -0700

    Updated elfinder init to dynamically load i18n language file

--- cms/modules/elfinder/init.php
+++ cms/modules/elfinder/init.php
@@ -1,8 +1,10 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
 
-Assets_Package::add('elfinder')
-	->js('elfinder.lib', ADMIN_RESOURCES . 'libs/elfinder/js/elfinder.min.js', 'global')
-	->js('elfinder.ru', ADMIN_RESOURCES . 'libs/elfinder/js/i18n/elfinder.ru.js', 'elfinder.lib')
-	->js('elfinder', ADMIN_RESOURCES . 'js/elfinder.js', 'global')
-	->css('elfinder.lib', ADMIN_RESOURCES . 'libs/elfinder/css/elfinder.min.css')
-	->css('elfinder', ADMIN_RESOURCES . 'css/elfinder.css', 'elfinder.lib');
+Observer::observe('modules::afer_load', function() {
+	$i18n_lang = substr(I18n::lang(), 0, 2);
+	Assets_Package::add('elfinder')
+		->js('elfinder.lib', ADMIN_RESOURCES . 'libs/elfinder/js/elfinder.min.js', 'global')
+		->js('elfinder.'.$i18n_lang, ADMIN_RESOURCES . 'libs/elfinder/js/i18n/elfinder.'.$i18n_lang.'.js', 'elfinder.lib')
+		->css('elfinder.lib', ADMIN_RESOURCES . 'libs/elfinder/css/elfinder.min.css')
+		->css('elfinder', ADMIN_RESOURCES . 'css/elfinder.css', 'elfinder.lib');
+});
