0ccafd37c582f440c18a5658c9820b87ccb56028
butschster <butschster@gmail.com>
2014-03-26 12:53:09 +0400

    Исправлена локализация плагина RedactorJS

--- cms/plugins/redactor/backend.php
+++ cms/plugins/redactor/backend.php
@@ -1,9 +1,11 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
 
-Assets_Package::add('redactor')
-	->js('redactor.ru', PLUGINS_URL . $plugin->id() . '/vendors/redactor/ru.js', 'jquery')
-	->js('redactor.min', PLUGINS_URL . $plugin->id() . '/vendors/redactor/redactor.js', 'jquery')
-	->js('redactor', ADMIN_RESOURCES . 'js/redactor.js', 'global')
-	->css('redator', PLUGINS_URL . $plugin->id() . '/vendors/redactor/redactor.css');
+Observer::observe('modules::afer_load', function($plugin) {
+	Assets_Package::add('redactor')
+		->js('redactor.' . I18n::lang(), PLUGINS_URL . $plugin->id() . '/vendors/redactor/'.I18n::lang().'.js', 'jquery')
+		->js('redactor.min', PLUGINS_URL . $plugin->id() . '/vendors/redactor/redactor.js', 'jquery')
+		->js('redactor', ADMIN_RESOURCES . 'js/redactor.js', 'global')
+		->css('redator', PLUGINS_URL . $plugin->id() . '/vendors/redactor/redactor.css');
 
-WYSIWYG::add('redactor');
+	WYSIWYG::add('redactor');
+}, $plugin);
