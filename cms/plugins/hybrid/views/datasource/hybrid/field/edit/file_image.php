415342fd4ce9558e96699f496424a44543166d38
butschster <butschster@gmail.com>
2014-04-01 17:25:29 +0400

    Исправлена ошибка с выбором максимального размера загружаемой картинки.

--- cms/plugins/hybrid/views/datasource/hybrid/field/edit/file_image.php
+++ cms/plugins/hybrid/views/datasource/hybrid/field/edit/file_image.php
@@ -2,17 +2,12 @@
 	<label class="control-label"><?php echo __( 'Max file size' ); ?></label>
 	<div class="controls">
 		<?php echo Form::input('max_size', $field->max_size, array('class' => 'input-small', 'id' => 'max_size')); ?> (<?php echo Text::bytes($field->max_size); ?>)
-		<span class="label file-size-label" data-size="<?php echo NUM::bytes('100K'); ?>">100k</span>
-		<span class="label file-size-label" data-size="<?php echo NUM::bytes('1MiB'); ?>">1Mib</span>
-		<span class="label file-size-label" data-size="<?php echo NUM::bytes('5MiB'); ?>">5Mib</span>
-		<span class="label file-size-label" data-size="<?php echo NUM::bytes('10MiB'); ?>">10Mib</span>
-		<script>
-		$(function() {
-			$('.file-size-label').click(function() {
-				$("#max_size").val($(this).data('size'));
-			});
-		})
-		</script>
+		<span class="flags">
+			<span class="label" data-value="<?php echo NUM::bytes('100K'); ?>">100k</span>
+			<span class="label" data-value="<?php echo NUM::bytes('1MiB'); ?>">1Mib</span>
+			<span class="label" data-value="<?php echo NUM::bytes('5MiB'); ?>">5Mib</span>
+			<span class="label" data-value="<?php echo NUM::bytes('10MiB'); ?>">10Mib</span>
+		</span>
 	</div>
 </div>
 
