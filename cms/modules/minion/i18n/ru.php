ae56ff1a294617bd1298688c4c3dcd69194574b5
Warren E. White <wwhite@nerret-tech.com>
2014-04-02 00:16:33 -0700

    Added minion Russian translation.

---
+++ cms/modules/minion/i18n/ru.php
@@ -0,0 +1,5 @@
+<?php defined( 'SYSPATH' ) or die( 'No direct access allowed.' );
+
+return array(
+		':field is not a valid option for this task!' => ':field не является допустимым вариантом для этой задачи!'
+);
