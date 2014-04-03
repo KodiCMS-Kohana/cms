b689fe91077815aeaec59d19a978cf253c212b52
butschster <butschster@gmail.com>
2014-04-03 17:53:18 +0400

    Исправлен механизм получения параметров

--- cms/plugins/hybrid/classes/controller/api/datasource/hybrid/field.php
+++ cms/plugins/hybrid/classes/controller/api/datasource/hybrid/field.php
@@ -4,9 +4,9 @@ class Controller_Api_Datasource_Hybrid_Field extends Controller_System_API
 {
 	public function rest_delete()
 	{
-		$ds_id = (int) $this->request->post('ds_id');
+		$ds_id = (int) $this->param('ds_id', NULL, TRUE);
 		
-		$fields = $this->request->post('field');
+		$fields = $this->param('field', array(), TRUE);
 		
 		$ds = Datasource_Data_Manager::load($ds_id);
 		DataSource_Hybrid_Field_Factory::remove_fields($ds->record(), $fields);
