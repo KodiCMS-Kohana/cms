f6bbb3d467114a74dd8bf6218e16530986a70ff4
butschster <butschster@gmail.com>
2014-04-09 12:06:47 +0400

    Добавлено описание методов класса i18n Добавлен метод получения короткой записи текущей локали `I18n::lang_short()`

--- cms/modules/elfinder/init.php
+++ cms/modules/elfinder/init.php
@@ -1,10 +1,9 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
 
 Observer::observe('modules::afer_load', function() {
-	$i18n_lang = substr(I18n::lang(), 0, 2);
 	Assets_Package::add('elfinder')
 		->js('elfinder.lib', ADMIN_RESOURCES . 'libs/elfinder/js/elfinder.min.js', 'global')
-		->js('elfinder.'.$i18n_lang, ADMIN_RESOURCES . 'libs/elfinder/js/i18n/elfinder.'.$i18n_lang.'.js', 'elfinder.lib')
+		->js('elfinder.'.I18n::lang_short(), ADMIN_RESOURCES . 'libs/elfinder/js/i18n/elfinder.'.I18n::lang_short().'.js', 'elfinder.lib')
 		->css('elfinder.lib', ADMIN_RESOURCES . 'libs/elfinder/css/elfinder.min.css')
 		->css('elfinder', ADMIN_RESOURCES . 'css/elfinder.css', 'elfinder.lib');
 });
