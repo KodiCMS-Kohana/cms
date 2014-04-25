4ade2b6695d5523e8e19a5c62c6dec9d8788d7d0
butschster <butschster@gmail.com>
2014-04-25 12:50:26 +0400

    Update version

--- cms/application/bootstrap.php
+++ cms/application/bootstrap.php
@@ -120,7 +120,7 @@ Kohana::init( array(
 
 define('CMS_NAME',			'KodiCMS');
 define('CMS_SITE',			'http://www.kodicms.ru');
-define('CMS_VERSION',		'10.02.17');
+define('CMS_VERSION',		'10.04.21');
 
 define('PUBLICPATH',		DOCROOT . 'public' . DIRECTORY_SEPARATOR);
 define('TMPPATH',			PUBLICPATH . 'temp' . DIRECTORY_SEPARATOR);
