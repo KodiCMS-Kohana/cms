c7ef9333f3ccfe31feecb708b658fb06b8b352f2
butschster <butschster@gmail.com>
2014-04-25 13:32:07 +0400

    Удаление ненужных контроллеров

--- cms/plugins/disqus/classes/controller/disqus.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-/**
- * @package		KodiCMS/Disqus
- * @category	Controller
- * @author		ButscHSter
- */
-class Controller_Disqus extends Controller_System_Plugin {}
