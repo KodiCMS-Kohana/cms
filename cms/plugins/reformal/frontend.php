2c8d34163179de8206080d36df0ce0abc0d8f719
butschster <butschster@gmail.com>
2014-04-25 13:32:52 +0400

    Remove plugin reformal

--- cms/plugins/reformal/frontend.php
+++
@@ -1,7 +0,0 @@
-<?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
-
-Observer::observe( 'page_layout_bottom', function($plugin) {
-	echo View::factory( 'reformal/footer', array(
-		'plugin' => $plugin
-	) );
-}, $plugin);
