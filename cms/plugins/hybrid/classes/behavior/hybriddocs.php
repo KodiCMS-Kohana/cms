73c361415e39d566b74521607f6ce704de501b88
Butschster <butschster@gmail.com>
2014-06-26 17:50:12 +0400

    Если указать типа страницы "гибридные документы" и указать страницу документа, то при создании еще одной внутренней страницы на нее не возможно было попасть

--- cms/plugins/hybrid/classes/behavior/hybriddocs.php
+++ cms/plugins/hybrid/classes/behavior/hybriddocs.php
@@ -2,37 +2,53 @@
 
 class Behavior_HybridDocs extends Behavior_Abstract
 {
-	
 	public function routes()
 	{
 		return array(
 			'/tag/<tag>' => array(
-				'method' => 'execute'
+				'regex' => array(
+					'tag' => '.*'
+				),
+				'method' => 'stub'
 			),
 			'/category/<category_id>' => array(
-				'method' => 'execute'
+				'regex' => array(
+					'category_id' => '[0-9]+'
+				),
+				'method' => 'stub'
 			),
 			'/<item>' => array(
+				'regex' => array(
+					'item' => '.*'
+				),
 				'method' => 'execute'
 			),
 		);
 	}
+	
+	public function stub(){}
 
 	public function execute()
 	{
 		$slug = $this->router()->param('item');
-		if( empty($slug) ) return;
-
-		$item_page_id = $this->settings()->item_page_id;
-		if(!empty($item_page_id))
+		
+		if(empty($slug))
 		{
-			$this->_page = Model_Page_Front::findById($this->settings()->item_page_id);
 			return;
 		}
 		
+		// Если не найдена внутрення страница по SLUG
 		if(($this->_page = Model_Page_Front::findBySlug($slug, $this->page())) === FALSE )
 		{
-            Model_Page_Front::not_found();
+			// Производим поиск страницы которая укзана в настройках типа страницы
+			$item_page_id = $this->settings()->item_page_id;
+			if( ! empty($item_page_id))
+			{
+				$this->_page = Model_Page_Front::findById($this->settings()->item_page_id);
+				return;
+			}
+
+			Model_Page_Front::not_found();
 		}
 	}
 }
