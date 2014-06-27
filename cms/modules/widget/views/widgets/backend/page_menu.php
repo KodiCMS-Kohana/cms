a9a6fd1a1cb6ff90342a99edcaa2a9fc57febbbe
Warren E. White <wwhite@nerret-tech.com>
2014-06-27 10:53:28 +0400

    Added missing closing label tag.

--- cms/modules/widget/views/widgets/backend/page_menu.php
+++ cms/modules/widget/views/widgets/backend/page_menu.php
@@ -46,7 +46,7 @@
 				<td>
 					<?php echo Form::checkbox('exclude[]', $page['id'], in_array($page['id'], $widget->exclude), array('id' => 'page'.$page['id'])); ?>
 				</td>
-				<th><label for="page<?php echo $page['id']; ?>"><?php echo str_repeat('&nbsp;', $page['level'] * 10) . $page['title']; ?></th>
+				<th><label for="page<?php echo $page['id']; ?>"><?php echo str_repeat('&nbsp;', $page['level'] * 10) . $page['title']; ?></label></th>
 				<?php else: ?>
 				<td></td>
 				<th><h4><?php echo $page['title']; ?></h4></th>
