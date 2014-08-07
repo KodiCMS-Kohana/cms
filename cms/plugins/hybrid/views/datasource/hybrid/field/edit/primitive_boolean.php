0690d2ea3bd32b8c1b4f96d4e884cdb94e17acfd
Butschster <butschster@gmail.com>
2014-08-07 20:21:53 +0400

    Добавлен шаблон редактоирования поля типа Boolean

---
+++ cms/plugins/hybrid/views/datasource/hybrid/field/edit/primitive_boolean.php
@@ -0,0 +1,16 @@
+<div class="control-group">
+	<label class="control-label"><?php echo __('Design'); ?></label>
+	<div class="controls">
+		<?php echo Form::select('display', $field->display_types(), $field->display); ?>
+	</div>
+</div>
+<hr />
+<div class="control-group">
+	<label class="control-label"><?php echo __('Default value'); ?></label>
+	<div class="controls">
+		<?php echo Form::select('default', array(
+			0 => __('No'),
+			1 => __("Yes")
+		), $field->default); ?>
+	</div>
+</div>
