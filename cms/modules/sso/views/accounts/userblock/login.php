55798aaed033ec84cfea5259ebf78ba4df40022a
Butschster <butschster@gmail.com>
2014-08-17 23:15:45 +0400

    update

--- cms/modules/sso/views/accounts/userblock/login.php
+++ cms/modules/sso/views/accounts/userblock/login.php
@@ -1,17 +1,22 @@
 <?php if(count($providers) > 0): ?>
-<hr />
-<h4><?php echo __('Sign-in with your social network'); ?></h4>
-<div class="btn-group">
-	<?php foreach ($providers as $provider => $data): ?>
-	<?php echo UI::button(Arr::path($params, $provider.'.name'), array(
-		'icon' => UI::icon($provider),
-		'class' => 'btn btn-inverse',
-		'href' => Route::get('accounts-auth')->uri(array(
-			
-			'directory' => 'oauth', 
-			'controller' => $provider, 
-			'action' => 'login'))
-	)) ?>
-	<?php endforeach; ?>
+<div class="panel margin-sm-vr">
+	<div class="panel-heading">
+		<span class="panel-title"><?php echo __('Sign-in with your social network'); ?></span>
+	</div>
+	<div class="panel-body">
+		<div class="btn-group">
+			<?php foreach ($providers as $provider => $data): ?>
+			<?php echo UI::button(Arr::path($params, $provider.'.name'), array(
+				'icon' => UI::icon($provider),
+				'class' => 'btn btn-inverse',
+				'href' => Route::get('accounts-auth')->uri(array(
+
+					'directory' => 'oauth', 
+					'controller' => $provider, 
+					'action' => 'login'))
+			)) ?>
+			<?php endforeach; ?>
+		</div>
+	</div>
 </div>
 <?php endif; ?>
