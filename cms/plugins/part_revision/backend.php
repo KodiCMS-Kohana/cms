aea1891018634f59fd8de2fdd6eccace30b67d8d
Butschster <butschster@gmail.com>
2014-08-30 12:24:25 +0400

    fix

--- cms/plugins/part_revision/backend.php
+++ cms/plugins/part_revision/backend.php
@@ -1,13 +1,16 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
 
 Observer::observe('part_before_save', function($part) {
-
+	if (!$part->loaded())
+	{
+		return;
+	}
+	
 	$data = array(
 		'part_id' => $part->id, 
 		'created_on' => date('Y-m-d H:i:s'),
 		'content' => Arr::get($part->original_values(), 'content')
 	);
-
 	DB::insert('part_revision')
 		->columns(array_keys($data))
 		->values($data)
