53dcc2d956ce0d96e6d95d89c5a747c6898f5a47
Butschster <butschster@gmail.com>
2014-08-31 02:15:03 +0400

    Page_not_found plugin - minimal 404 error response for asset files. Fix by @igk1972

--- cms/plugins/page_not_found/classes/http/exception/front/404.php
+++ cms/plugins/page_not_found/classes/http/exception/front/404.php
@@ -13,17 +13,32 @@ class HTTP_Exception_Front_404 extends Kohana_HTTP_Exception_404
 		
 		if( ! empty($page_slug) )
 		{
-			$page = Model_Page_Front::find( $page_slug );
+			$page = Model_Page_Front::find($page_slug);
 
-			if( $page instanceof Model_Page_Front )
+			if ($page instanceof Model_Page_Front)
 			{
+				$ext = pathinfo(Request::current()->url(), PATHINFO_EXTENSION);
+				$mimetype = FALSE;
+
+				if ($ext AND ! ($mimetype = File::mime_by_ext($ext)))
+				{
+					$mimetype = 'application/octet-stream';
+				}
+
+				if ($mimetype)
+				{
+					return Response::factory()
+						->headers('content-type', $mimetype)
+						->status(404);
+				}
+
 				return Request::factory($page->url)
 					->query('message', $this->message)
 					->execute()
 					->status(404);
 			}
 		}
-		
+
 		throw new HTTP_Exception_404('Something went wrong');
 	}
 }
