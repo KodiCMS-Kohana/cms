53b75e86cd4d2c8b7da0738e3c704fa5afcb5678
Igor Kuznetsov <igk@igk.ru>
2014-09-01 19:34:34 +0400

    Dashboard - add permissions

---
+++ cms/modules/dashboard/config/permissions.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'dashboard' => array(
+		array(
+			'action' => 'index',
+			'description' => 'View dashboard'
+		),
+	),
+);
