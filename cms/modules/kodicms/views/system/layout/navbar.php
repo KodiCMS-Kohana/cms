ba56ceb660537b9f2c7972282cde4b64ac3c7021
Butschster <butschster@gmail.com>
2014-09-01 20:06:24 +0400

    Улучшен внешний вид выпадающего меню профиля

--- cms/modules/kodicms/views/system/layout/navbar.php
+++ cms/modules/kodicms/views/system/layout/navbar.php
@@ -11,35 +11,41 @@
 			<div>
 				<div class="right clearfix">
 					<ul class="nav navbar-nav pull-right right-navbar-nav">
-						
 						<?php Observer::notify('view_navbar_menu'); ?>
-
 						<li>
 							<?php echo HTML::anchor(Route::get('backend')->uri(array('controller' => 'system', 'action' => 'settings')), UI::icon('cogs fa-lg')); ?>
 						</li>
-						
 						<li>
 							<?php echo HTML::anchor(URL::base(TRUE), UI::hidden(__('View Site')), array(
 								'target' => 'blank', 'data-icon' => 'globe fa-lg text-info'
 							)); ?>
 						</li>
-						<li class="dropdown">
+						<li class="dropdown user-menu">
 							<a href="#" class="dropdown-toggle user-menu" data-toggle="dropdown">
 								<?php echo Auth::get_record()->gravatar(25); ?>
 								<span><?php echo Auth::get_username(); ?></span>
 							</a>
 							<ul class="dropdown-menu">
-								<li>
-									<?php echo HTML::anchor(Route::get('backend')->uri(array('controller' => 'users', 'action' => 'profile')), __('Profile'), array('data-icon' => 'user')); ?>
+								<li class="user-header">
+									<?php echo Auth::get_record()->gravatar(90, NULL, array(
+										'class' => 'img-circle'
+									)); ?>
+									
+									<p><?php echo Auth::get_record()->profile->name; ?> <small><?php echo Auth::get_record()->email; ?></small></p>
+									
 								</li>
-								<li>
-									<?php echo HTML::anchor(Route::get('backend')->uri(array('controller' => 'users', 'action' => 'edit', 'id' => Auth::get_id())), __('Settings'), array('data-icon' => 'cog')); ?>
-								</li>
-								<li class="divider"></li>
-								<li>
+								<li class="user-body">
+									<div class="col-xs-6">
+										<?php echo HTML::anchor(Route::get('backend')->uri(array('controller' => 'users', 'action' => 'profile')), __('Profile'), array('data-icon' => 'user')); ?>
+									</div>
+									<div class="col-xs-6">
+										<?php echo HTML::anchor(Route::get('backend')->uri(array('controller' => 'users', 'action' => 'edit', 'id' => Auth::get_id())), __('Settings'), array('data-icon' => 'cog')); ?>
+									</div>
+                                </li>
+								<li class="user-footer">
 									<?php echo HTML::anchor(Route::get('user')->uri(array('action' => 'logout')), __('Logout'), array(
 										'data-icon' => 'power-off text-danger',
-										'class' => 'text-bold'
+										'class' => 'btn btn-default btn-xs text-bold pull-right'
 									)); ?>
 								</li>
 							</ul>
