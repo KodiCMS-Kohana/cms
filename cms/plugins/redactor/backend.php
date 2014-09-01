01ee01fd63dd30c85dee8d0ede10e6c269f2e8c5
Butschster <butschster@gmail.com>
2014-09-01 20:47:00 +0400

    Перенос папки плагина redactor - vendor в папку media и исправлены пути подключения медиа файлов плагина

--- cms/plugins/redactor/backend.php
+++ cms/plugins/redactor/backend.php
@@ -2,10 +2,10 @@
 
 Observer::observe('modules::after_load', function($plugin) {
 	Assets_Package::add('redactor')
-		->js('redactor.' . I18n::lang(), PLUGINS_URL . $plugin->id() . '/vendors/redactor/'.I18n::lang().'.js', 'jquery')
-		->js('redactor.min', PLUGINS_URL . $plugin->id() . '/vendors/redactor/redactor.min.js', 'jquery')
+		->js('redactor.' . I18n::lang(), ADMIN_RESOURCES . 'vendors/redactor/'.I18n::lang().'.js', 'jquery')
+		->js('redactor.min', ADMIN_RESOURCES . 'vendors/redactor/redactor.min.js', 'jquery')
 		->js('redactor', ADMIN_RESOURCES . 'js/redactor.js', 'global')
-		->css('redator', PLUGINS_URL . $plugin->id() . '/vendors/redactor/redactor.css');
+		->css('redator', ADMIN_RESOURCES . 'vendors/redactor/redactor.css');
 
 	WYSIWYG::add('redactor');
 }, $plugin);
