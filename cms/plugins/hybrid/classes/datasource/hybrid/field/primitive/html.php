f503b2998b7218b130c1f7640e9cc758aad6899c
Butschster <butschster@gmail.com>
2014-09-04 21:11:42 +0400

    Fix

--- cms/plugins/hybrid/classes/datasource/hybrid/field/primitive/html.php
+++ cms/plugins/hybrid/classes/datasource/hybrid/field/primitive/html.php
@@ -9,32 +9,32 @@ class DataSource_Hybrid_Field_Primitive_HTML extends DataSource_Hybrid_Field_Pri
 		'filter_html' => FALSE,
 		'allowed_tags' => '<b><i><u><p><ul><li><ol>'
 	);
-	
+
 	public function booleans()
 	{
 		return array('filter_html');
 	}
-	
-	public function set( array $data )
-	{	
-		return parent::set( $data );
-	}
-	
-	public function onUpdateDocument( DataSource_Hybrid_Document $old = NULL, DataSource_Hybrid_Document $new ) 
+
+	public function set(array $data)
 	{
-		if( $this->filter_html === TRUE )
+		return parent::set($data);
+	}
+
+	public function onUpdateDocument(DataSource_Hybrid_Document $old = NULL, DataSource_Hybrid_Document $new)
+	{
+		if ($this->filter_html === TRUE)
 		{
-			$new->set($this->name, Kses::filter( $new->get($this->name), $this->allowed_tags ));
+			$new->set($this->name, Kses::filter($new->get($this->name), $this->allowed_tags));
 		}
 	}
-	
-	public function get_type() 
+
+	public function get_type()
 	{
 		return 'TEXT NOT NULL';
 	}
-	
-	public function fetch_headline_value( $value, $document_id )
+
+	public function fetch_headline_value($value, $document_id)
 	{
-		return substr(strip_tags($value), 0, 500) . ' ...';
+		return Text::limit_words($value, 50);
 	}
 }
