5dceea3a9a91cdb30f7b10371592c2e78b0978a3
Butschster <butschster@gmail.com>
2014-09-09 09:27:02 +0400

    Fix

--- cms/modules/widget/views/widgets/edit.php
+++ cms/modules/widget/views/widgets/edit.php
@@ -135,8 +135,9 @@
 	<?php echo $content; ?>
 	<?php if($widget->is_handler()): ?>
 	<div class="alert alert-danger note-dark no-margin-b">
-		<?php echo __('To use handler send your data to URL :href', array(
-			':href' => HTML::anchor($widget->link(), URL::site($widget->link(), TRUE))
+		<?php echo __('To use handler send your data to URL :href or use route :route', array(
+			':href' => '<code>' . URL::site($widget->link(), TRUE) . '</code>',
+			':route' => '<code>Route::get(\'handler\')->uri(array(\'id\' => ' .$widget->id. '));</code>'
 		)); ?>
 	</div>
 	<?php endif; ?>
