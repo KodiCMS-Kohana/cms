2c408ae0e85bf15694410761593ac6c3da98ca8a
Butschster <butschster@gmail.com>
2014-09-13 00:44:45 +0400

    FIx

--- cms/plugins/hybrid/views/datasource/hybrid/document/edit.php
+++ cms/plugins/hybrid/views/datasource/hybrid/document/edit.php
@@ -177,7 +177,7 @@ function update_documents(e, response) {
 }
 </script>
 
-<?php if(!$document->has_access_change()): ?>
+<?php if($document->has_access_change()): ?>
 <?php echo Form::open(Route::get('datasources')->uri(array(
 		'controller' => 'document',
 		'directory' => $datasource->type(),
@@ -246,7 +246,7 @@ function update_documents(e, response) {
 </div>
 <?php endif; ?>
 
-<?php if(!$document->has_access_change()): ?>
+<?php if($document->has_access_change()): ?>
 <div class="form-actions panel-footer">
 	<?php echo UI::actions(TRUE, Route::get('datasources')->uri(array(
 		'controller' => 'data',
