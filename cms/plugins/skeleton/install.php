55cebb5f48b157e5cd6dc57b8333af8d536011a9
butschster <butschster@gmail.com>
2014-10-22 14:06:44 +0400

    issue #289

---
+++ cms/plugins/skeleton/install.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// Этот файл загружается после выполения SQL кода из файла install/shcema.sql
+// Наличие данного файла не обязательно
