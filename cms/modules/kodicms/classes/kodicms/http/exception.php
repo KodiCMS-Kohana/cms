f5132cc552927ddad37efdca2c511b38aa8b7209
Butschster <butschster@gmail.com>
2014-10-22 23:49:13 +0400

    Fix

--- cms/modules/kodicms/classes/kodicms/http/exception.php
+++ cms/modules/kodicms/classes/kodicms/http/exception.php
@@ -20,7 +20,7 @@ class KodiCMS_HTTP_Exception extends Kohana_HTTP_Exception {
 		// Lets log the Exception, Just in case it's important!
 		Kohana_Exception::log($this);
 
-		if ( Config::get('site', 'debug') == Config::YES)
+		if (Config::get('site', 'debug') == Config::YES)
 		{
 			// Show the normal Kohana error page.
 			return parent::get_response();
@@ -49,7 +49,7 @@ class KodiCMS_HTTP_Exception extends Kohana_HTTP_Exception {
 					->status($this->getCode())
 					->body($request);
 			}
-			catch ( Exception $e )
+			catch (Exception $e)
 			{
 				return parent::get_response();
 			}
