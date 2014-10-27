d27e56e23319cc92fa9faa69d5e6c824e9e5b625
Butschster <butschster@gmail.com>
2014-10-27 23:26:31 +0300

    Исправлены ошибки в JavaScript коде плагина Page Fields и поправлен шаблон Также добавлена валидация ключа поля на уникальность. thx to @timoffei

--- cms/plugins/page_fields/classes/kodicms/controller/api/page/field.php
+++ cms/plugins/page_fields/classes/kodicms/controller/api/page/field.php
@@ -33,29 +33,31 @@ class KodiCMS_Controller_API_Page_Field extends Controller_System_Api {
 		
 		$page = ORM::factory('page', $page_id);
 		
-		if( ! $page->loaded() )
-			throw new HTTP_API_Exception(__('Page not found'));
-		
-		if( !empty($field_array['field_id']))
+		if (!$page->loaded())
 		{
-			$field = ORM::factory( 'page_field', (int) $field_array['field_id']);
-			
-			if( ! $field->loaded())
+			throw new HTTP_API_Exception(__('Page not found'));
+		}
+
+		if (!empty($field_array['field_id']))
+		{
+			$field = ORM::factory('page_field', (int) $field_array['field_id']);
+
+			if (!$field->loaded())
 			{
 				throw new HTTP_API_Exception(__('Field not found'));
 			}
-			
+
 			$field_array['key'] = $field->key;
 			$field_array['title'] = $field->title;
-			
+
 			unset($field_array['field_id'], $field);
 		}
 
 		$field_array['page_id'] = $page_id;
-		
-		$field = ORM::factory('page_field')->values($field_array);
-		
-		$field->create();
+
+		$field = ORM::factory('page_field')
+			->values($field_array)
+			->create();
 
 		$view = View::factory('page/fields/field', array(
 			'field' => $field
@@ -67,9 +69,9 @@ class KodiCMS_Controller_API_Page_Field extends Controller_System_Api {
 	public function rest_delete()
 	{
 		$field_id = (int) $this->param('field_id', NULL, TRUE);
-		
+
 		ORM::factory('page_field', $field_id)->delete();
-		
+
 		$this->message('Page field deleted');
 	}
 	
@@ -81,16 +83,9 @@ class KodiCMS_Controller_API_Page_Field extends Controller_System_Api {
 		$field_id = (int) $this->param('field_id', NULL, TRUE);
 		$value = $this->param('value');
 
-		try 
-		{
-			ORM::factory('page_field', $field_id)->values(array(
-				'value' => $value
-			))->update();
-		} 
-		catch (ORM_Validation_Exception $v) 
-		{
-			$this->json['message'] = $v->errors('validation');
-		}		
+		ORM::factory('page_field', $field_id)->values(array(
+			'value' => $value
+		))->update();
 	}
 	
 }
--- cms/plugins/page_fields/views/page/fields/field.php
+++ cms/plugins/page_fields/views/page/fields/field.php
@@ -1,13 +1,13 @@
 <div class="page-field row" data-id="<?php echo $field->id; ?>">
 	<div class="col-md-3 system-field">
 		<?php if($field->loaded()): ?>
-		<?php echo FORM::input('title', $field->title, array(
+		<?php echo Form::input('title', $field->title, array(
 			'placeholder' => __('Field title'), 'data-slug' => '.field-slug',
 			'class' => 'form-control', 'disabled'
 		)); ?>
 		<?php else: ?>
 
-		<?php echo FORM::input('title', $field->title, array(
+		<?php echo Form::input('title', $field->title, array(
 			'placeholder' => __('Field title'), 'data-slug' => '.field-slug',
 			'class' => 'form-control'
 		)); ?>
@@ -16,12 +16,12 @@
 	
 	<div class="col-md-2 system-field">
 		<?php if($field->loaded()): ?>
-		<?php echo FORM::input('key', $field->key, array(
+		<?php echo Form::input('key', $field->key, array(
 			'placeholder' => __('Field key'), 'disabled',
 			'class' => 'form-control slug field-slug', 'data-separator' => '_'
 		)); ?>
 		<?php else: ?>
-		<?php echo FORM::input('key', $field->key, array(
+		<?php echo Form::input('key', $field->key, array(
 			'placeholder' => __('Field key'), 
 			'class' => 'form-control slug field-slug', 'data-separator' => '_'
 		)); ?>
@@ -30,18 +30,18 @@
 
 	<div class="col-md-7">
 		<div class="input-group">
-			<?php echo FORM::input('value', $field->value, array(
+			<?php echo Form::input('value', $field->value, array(
 				'placeholder' => (empty($field->value) AND $field->loaded()) ? '' : __('Field value'), 
 				'class' => 'form-control', 'data-filemanager' => 'true'
 			)); ?>
 
 			<div class="input-group-btn">
 				<?php if($field->loaded()): ?>
-				<?php echo FORM::button('remove_field', UI::icon( 'trash-o'), array(
+				<?php echo Form::button('remove_field', UI::icon( 'trash-o'), array(
 					'class' => 'btn btn-danger btn-remove'
 				)); ?>
 				<?php else: ?>
-				<?php echo FORM::button('add_field', UI::icon( 'plus'), array(
+				<?php echo Form::button('add_field', UI::icon( 'plus'), array(
 					'class' => 'btn btn-success btn-add'
 				)); ?>
 				<?php endif; ?>
