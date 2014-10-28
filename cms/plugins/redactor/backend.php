fdaade81cec07afd46cc94def85f31cca9564c6a
Butschster <butschster@gmail.com>
2014-10-28 09:19:55 +0300

    Исправлен дизайн модальных окон в плагине RedactorJS, а также вынос инициализации пакета скриптов в `init.php`

--- cms/plugins/redactor/backend.php
+++ cms/plugins/redactor/backend.php
@@ -1,11 +1,5 @@
 <?php defined( 'SYSPATH' ) or die( 'No direct script access.' );
 
-Observer::observe('modules::after_load', function($plugin) {
-	Assets_Package::add('redactor')
-		->js('redactor.' . I18n::lang(), ADMIN_RESOURCES . 'vendors/redactor/'.I18n::lang().'.js', 'jquery')
-		->js('redactor.min', ADMIN_RESOURCES . 'vendors/redactor/redactor.min.js', 'jquery')
-		->js('redactor', ADMIN_RESOURCES . 'js/redactor.js', 'global')
-		->css('redator', ADMIN_RESOURCES . 'vendors/redactor/redactor.css');
-
+Observer::observe('modules::after_load', function() {
 	WYSIWYG::add('redactor');
-}, $plugin);
+});
