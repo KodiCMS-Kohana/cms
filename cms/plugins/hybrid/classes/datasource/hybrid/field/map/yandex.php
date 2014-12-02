b37b6eab58c29c49f1fb650fdc72d2349ccd3090
Butschster <butschster@gmail.com>
2014-12-02 14:47:31 +0300

    Изменение месторасположения ГД поля "Яндекс карты". Если у вас было созданно такое поле, то необходимо исправить его ключ в таблице БД dsfields на map_yandex

--- cms/plugins/hybrid/classes/datasource/hybrid/field/map/yandex.php
+++ cms/plugins/hybrid/classes/datasource/hybrid/field/map/yandex.php
@@ -8,7 +8,7 @@
  * @copyright	(c) 2012-2014 butschster
  * @license		http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
  */
-class DataSource_Hybrid_Field_Yandex_Map extends DataSource_Hybrid_Field_Primitive {
+class DataSource_Hybrid_Field_Map_Yandex extends DataSource_Hybrid_Field_Primitive {
 	
 	protected $_is_searchable = FALSE;
 
@@ -20,33 +20,37 @@ class DataSource_Hybrid_Field_Yandex_Map extends DataSource_Hybrid_Field_Primiti
 		'default' => '55.753994, 37.622093'
 	);
 
-	public function convert_value( $value )
+	public function convert_value($value)
 	{
-		
-		if( is_string($value) )
+		if (is_string($value))
 		{
 			return explode(',', $value);
 		}
-		
+
 		return parent::convert_value($value);
 	}
-	
-	public function onCreateDocument( DataSource_Hybrid_Document $doc) 
+
+	public function onCreateDocument(DataSource_Hybrid_Document $doc)
 	{
 		$this->onUpdateDocument($doc, $doc);
 	}
-	
+
 	public function onUpdateDocument(DataSource_Hybrid_Document $old = NULL, DataSource_Hybrid_Document $new)
 	{
 		$value = $new->get($this->name);
-		if(is_array($value))
+	
+		if (is_array($value))
 		{
-			if(!empty($value[0]) AND !empty($value[1]))
+			if (!empty($value[0]) AND ! empty($value[1]))
+			{
 				$value = implode(',', $value);
+			}
 			else
+			{
 				$value = '';
+			}
 		}
-		
+
 		$new->set($this->name, $value);
 	}
 
@@ -55,44 +59,45 @@ class DataSource_Hybrid_Field_Yandex_Map extends DataSource_Hybrid_Field_Primiti
 		return 'VARCHAR (50)';
 	}
 	
-	public function coord_x( $value )
+	public function coord_x($value)
 	{
-		if( ! is_array($value) AND strpos($value, ',') !== FALSE)
+		if (!is_array($value) AND strpos($value, ',') !== FALSE)
 		{
 			$value = explode(',', $value, 2);
 		}
-		
+
 		$coord = NULL;
-		
-		if(is_array($value))
+
+		if (is_array($value))
 		{
 			$coord = Arr::get($value, 0);
 		}
-	
+
 		return $coord;
 	}
-	
-	public function coord_y( $value )
+
+	public function coord_y($value)
 	{
-		if( ! is_array($value) AND strpos($value, ',') !== FALSE)
+		if (!is_array($value) AND strpos($value, ',') !== FALSE)
 		{
 			$value = explode(',', $value, 2);
 		}
-		
+
 		$coord = NULL;
-		
-		if(is_array($value))
+
+		if (is_array($value))
 		{
 			$coord = Arr::get($value, 1);
 		}
 
 		return $coord;
 	}
-	
+
 	public function onControllerLoad()
 	{
 		Assets::js('Yandex.map', 'http://api-maps.yandex.ru/2.0/?load=package.full&lang=' . I18n::lang());
-		
+
 		parent::onControllerLoad();
 	}
+
 }
