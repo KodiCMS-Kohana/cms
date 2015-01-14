617f50d1752a0e18e15d5a29eb752af0bb3d1917
butschster <butschster@gmail.com>
2015-01-14 15:06:55 +0300

    Remove userguide module

--- cms/modules/userguide/classes/kodoc/markdown.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Kodoc_Markdown extends Kohana_Kodoc_Markdown {}
--- cms/modules/userguide/koharness.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Configuration for koharness - builds a standalone skeleton Kohana app for running unit tests
-return array(
-	'modules' => array(
-		'userguide' => __DIR__,
-		'unittest'  => __DIR__ . '/vendor/kohana/unittest'
-	),
-);
