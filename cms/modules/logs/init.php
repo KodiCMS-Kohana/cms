e92084d4839be4efea64ff355c7f0b588634b74b
Butschster <butschster@gmail.com>
2015-01-15 19:03:43 +0300

    fix

--- cms/modules/logs/init.php
+++ cms/modules/logs/init.php
@@ -2,7 +2,7 @@
 
 Observer::observe('view_user_profile_information', function($user_id) {
 	$logs = Api::get('log.get', array('uids' => $user_id, 'level' => Log::INFO))->as_object()->get('response');
-	echo View::factory('log/profile/activity', array(
+	echo View::factory('logs/profile/activity', array(
 		'user_id' => (int) $user_id,
 		'logs' => $logs
 	));
--- cms/modules/logs/views/logs/profile/activity.php
+++ cms/modules/logs/views/logs/profile/activity.php
@@ -1,20 +1,20 @@
-<?php if ( !empty($logs) ): ?>
+<?php if (!empty($logs)): ?>
 <div class="panel-heading">
 	<span class="panel-title"><?php echo __('Activity'); ?></span>
 </div>
-
-<div class="widget-article-comments panel-body no-border fade active in" id="profile-tabs-board">
+<div class="widget-comments panel-body no-padding" id="profile-tabs-board">
 	<?php foreach ($logs as $log): ?>
 	<div class="comment">
 		<?php echo Gravatar::load($log->email, 32, NULL, array(
 			'class' => 'comment-avatar'
 		)); ?>
 		<div class="comment-body">
+			<div class="comment-by">
+				<?php echo HTML::anchor(Route::get('backend')->uri(array('controller' => 'users', 'action' => 'profile', 'id' => $log->user_id)), $log->username); ?>
+				<span><?php echo Date::format($log->created_on, 'j F Y H:i'); ?></span>
+			</div>
 			<div class="comment-text">
-				<div class="comment-heading">
-					<?php echo HTML::anchor(Route::get('backend')->uri(array('controller' => 'users', 'action' => 'profile', 'id' => $log->user_id)), $log->username); ?>
-					<span><?php echo Date::format($log->created_on, 'j F Y H:i'); ?></span>
-				</div>
+				
 				<?php echo $log->message; ?>
 			</div>
 		</div>
