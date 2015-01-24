c7c3c1a905f7516a65f7e74e69e9f9c4825526d7
Butschster <butschster@gmail.com>
2015-01-24 12:35:10 +0300

    В шаблон по умолчанию для виджета "Меню" заменена ссылка на страницу с `url` на `url`, а так-же добавлен вывод связанного виджета.

--- cms/modules/widget/views/widgets/frontend/page_menu.php
+++ cms/modules/widget/views/widgets/frontend/page_menu.php
@@ -8,15 +8,19 @@
 
 <?php function recurse_page_menu($pages)
 {
-	
 	$return = '<ul class="nav">';
 	
 	foreach($pages as $p)
 	{
 		$return .= '<li>';
-		$return .= HTML::anchor($p['uri'], $p['title'], array(
+		$return .= HTML::anchor($p['url'], $p['title'], array(
 			'class' =>  $p['is_active'] ? 'current' : ''
 		));
+		
+		if (!empty($p['widget']) AND $p['widget'] instanceof Model_Widget_Decorator)
+		{
+			$return .= $p['widget']->render(array('return' => TRUE));
+		}
 		
 		if (!empty($p['childs']))
 		{
